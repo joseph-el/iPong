579aa953a7c49cd74555ab90dcf8d174a72d88ba
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-10 23:24:42 +0100

    feat: Add profile picture to SetUserNameAndPicture component
    Add profile picture selection and display to the SetUserNameAndPicture component.

--- frontend/srcs/components/AuthComponents/SetUserNameAndPicture/SetUserNameAndPicture.tsx
+++ frontend/srcs/components/AuthComponents/SetUserNameAndPicture/SetUserNameAndPicture.tsx
@@ -4,51 +4,84 @@ import InputComponent from '../../UI/Input/Input'
 import Close from '../../UI/Button/CloseButton/CloseButton'
 import CustomButton from '../../UI/Button/SubmitButton/SubmitButton'
 import {Avatar} from "@nextui-org/react";
+import GestUser from "../assets/gest_user.svg"
+import {CameraIcon} from '../../UI/icon/CameraIcon';
 
+import { useState } from "react";
 
 export const UserNameTitle = () => {
-
-    return (
-        <div className="date-of-birth">
-            <p className="this-will-not-be">
-                Your <span className='user-name'>@username</span> is unique. You can always change it later.
-            </p>
-        </div>
-    );
-
+  return (
+    <div className="user-name-alert">
+      <p className="this-will-not-be-user">
+        Your <span className="user-name">@username</span> is unique. You can
+        always change it later.
+      </p>
+    </div>
+  );
 };
 
 export default function SetUserNameAndPicture() {
-    return (
-            <div className="SetUserNameAndPicture-frame max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
-                <Close ClassName={"creat-close"}/>
+  const [selectedImage, setSelectedImage] = useState(null);
 
-                <div className='SetUserNameAndPicture'>
-               
-                    <div className="text-wrapper-3">Pick a profile pictures</div>
+  const handleImageChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        setSelectedImage(reader.result);
+      };
+      reader.readAsDataURL(file);
+    }
+  };
 
-                    <div className='picture-target'>
-                        <Avatar src="https://i.pravatar.cc/150?u=a04258114e29026708c" className="w-20 h-20 text-large" />
-                    </div>
+  return (
+    <div className="SetUserNameAndPicture-frame max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
+      <div className="SetUserNameAndPicture">
+        <Close ClassName={"creat-close"} />
 
+        <div className="text-wrapper-3">Pick a profile picture</div>
 
-                    <div className="text-wrapper-4">What should we call you?</div>
+        <div className="picture-target">
 
-                    <UserNameTitle/>
+          <Avatar src={!selectedImage ? GestUser : selectedImage} className="w-32 h-32 text-large" />
+       
+          <div className="camera-icon-padding">
+            <label htmlFor="fileInput">
+              <Avatar
+                className="camera-icon-size cursor-pointer"
+                showFallback
+                src="https://images.unsplash.com/broken"
+                fallback={
+                  <CameraIcon
+                    className="animate-pulse w-6 h-6 text-default-500"
+                    fill="currentColor"
+                    size={20}
+                  />
+                }
+              />
+            </label>
+            <input
+              type="file"
+              id="fileInput"
+              accept="image/*"
+              style={{ display: "none" }}
+              onChange={handleImageChange}
+            />
+          </div>
+        </div>
 
-                        <InputComponent type={"fill"} target={"username"}/>
-        
-                    <div className='buttons-target'>
-                            <CustomButton
-                                classNames="create-account"
-                                text="Next"
-                            />
-                    </div>
-                    
-                </div>
+        <div className="text-wrapper-4">What should we call you?</div>
 
-                
-                 
-            </div> 
-    );
+        <UserNameTitle />
+
+        <div className="input-padding">
+          <InputComponent type={"fill"} target={"username"} />
+        </div>
+
+        <div className="buttons-target">
+          <CustomButton classNames="create-account" text="Next" />
+        </div>
+      </div>
+    </div>
+  );
 }
