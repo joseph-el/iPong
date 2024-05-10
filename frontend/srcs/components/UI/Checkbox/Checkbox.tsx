90d57d91b3a9a86631b85d77b457db32ed56bbc6
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-10 20:09:26 +0100

    feat: Add a custom checkbox component to handle gender selection
    This commit introduces a new custom checkbox component (Checkbox.tsx) to handle gender selection in the UI.
    The component includes functionality to handle male and female options, with default selection and event handlers for gender changes.
    This allows for improved user experience and flexibility in gender selection.

--- frontend/srcs/components/UI/Checkbox/Checkbox.tsx
+++ frontend/srcs/components/UI/Checkbox/Checkbox.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import {Checkbox} from "@nextui-org/react";
+import './Checkbox.css'
+import { useState } from 'react';
 
 export default function CustomCheckbox() {
+  const [maleSelected, setMaleSelected] = useState(0);
+  
+  const handleMaleChange = () => {
+    setMaleSelected(1);
+  };
+
+  const handleFemaleChange = () => {
+    setMaleSelected(2);
+  };
+
+  
   return (
-    <div className="flex gap-1">
-      <Checkbox select-none >Option</Checkbox>
-      <Checkbox defaultSelected>Option</Checkbox>
+    <div className="flex gap-4">
+      <Checkbox className="Gender-type" defaultSelected={maleSelected == 1} onChange={handleMaleChange}>male</Checkbox>
+      <Checkbox className="Gender-type" defaultSelected={maleSelected == 2} onChange={handleFemaleChange}>female</Checkbox>
     </div>  
   );
 }
+
