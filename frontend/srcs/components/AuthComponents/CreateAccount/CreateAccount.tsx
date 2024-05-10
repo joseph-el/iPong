8cf87503e3aa16638419f98f8023cb437d20137b
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-10 15:29:53 +0100

    Refactor: Replace CustomButton with SubmitButton in CreateAccount
    This commit replaces the CustomButton component with the SubmitButton component in the CreateAccount component. It also adds a new competent class to the CreateAccount container, which sets the size and shape of the container.

--- frontend/srcs/components/AuthComponents/CreateAccount/CreateAccount.tsx
+++ frontend/srcs/components/AuthComponents/CreateAccount/CreateAccount.tsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import './CreateAccount.css'
-import CustomButton from '../../UI/Button/CustomButton'
+import CustomButton from '../../UI/Button/SubmitButton/SubmitButton'
 import InputComponet from '../../UI/Input/Input'
 
 
 export default function CreateAccount() {
     return (
         <>
+            <div className="CreateAccount-competent max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
 
+            </div>
         </>
     ); 
 }
