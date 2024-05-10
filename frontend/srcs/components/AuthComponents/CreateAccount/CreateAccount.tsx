28ae2d39f6c4984233ba0561b624873e4662ad25
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-10 14:58:07 +0100

    feat: Implement Create Account page
    Created a new Create Account page with a form to create a new user.

---
+++ frontend/srcs/components/AuthComponents/CreateAccount/CreateAccount.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './CreateAccount.css'
+import CustomButton from '../../UI/Button/CustomButton'
+import InputComponet from '../../UI/Input/Input'
+
+
+export default function CreateAccount() {
+    return (
+        <>
+
+        </>
+    ); 
+}
