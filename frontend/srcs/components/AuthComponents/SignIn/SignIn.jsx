c034d84edf119f9bb5b2dd879e0106771936d5cf
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-10 14:57:47 +0100

    feat(SignIn): Update styling and components
    Refactor SignIn component, introduce new components: Close, Label, LoginHelp, InputComponet.

--- frontend/srcs/components/AuthComponents/SignIn/SignIn.jsx
+++ frontend/srcs/components/AuthComponents/SignIn/SignIn.jsx
@@ -1,35 +1,59 @@
 import React from 'react'
 import './SignIn.css'
 import CustomButton from '../../UI/Button/CustomButton'
-import Effect from '../assets/maskgroup.svg'
+import CloseIcon from '../assets/Close.svg'
+import InputComponet from '../../UI/Input/Input'
+import MaskGroup from  '../assets/maskgroup.svg'
 
-
-export const Box = () => {
+export const Close = () => {
     return (
-        <div className="box">
-            <img className="mask-group" alt="Mask group" src={Effect} />
+        <div className="close">
+            <img className="SF-symbol-xmark" alt="Sf symbol xmark" src={CloseIcon} />
         </div>
     );
 };
-    
+
+export const Label = () => {
+    return (
+        <div className="label">
+            <div className="text-wrapper">Sign in to iPong</div>
+        </div>
+    );
+};
+
+export const LoginHelp = ({title, ClassName}) => {
+    return (
+        <div className={ClassName}>
+            <div className="left-title-help">{title}</div>
+        </div>
+    );
+};
 
 
 export default function SignIn() {
-
     return (
         <>
-            <div className="sign-in-competent max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden  h-[32rem] w-[25rem]   sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
-                <div className="title">
-                    <div className="group">
-                        <div className="div">Sign in to iPong</div>
-                    </div>
+            <div className="sign-in-competent max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
+                <div className="maskgroup-svg">
+                    <img src={MaskGroup} alt="Mask Group SVG" />
                 </div>
-         
-
+                <Close />
+                <Label />
+                <InputComponet type={"Email"} />
+                <InputComponet type={"pass"} />
+                <LoginHelp title={"Forgot password?"} ClassName={"reset-pass"} />
+                <LoginHelp title={"Create Account?"} ClassName={"create-acc"} />
+                <div className='buttons-target'>
+                    <CustomButton
+                        classNames="sign-in-competent-sign-in"
+                        text="Log in"
+                    />
+                </div>
             </div>
-        </>   
+        </>
     );
 }
+
 
 
 
