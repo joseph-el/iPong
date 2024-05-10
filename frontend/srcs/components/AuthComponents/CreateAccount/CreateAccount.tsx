375bcb623930a8bd88647aa2107edd503c9e4c6d
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-10 15:45:26 +0100

    feat: add close button to create account
    Adds a close button to the top right of the Create Account form.

--- frontend/srcs/components/AuthComponents/CreateAccount/CreateAccount.tsx
+++ frontend/srcs/components/AuthComponents/CreateAccount/CreateAccount.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import './CreateAccount.css'
 import CustomButton from '../../UI/Button/SubmitButton/SubmitButton'
 import InputComponet from '../../UI/Input/Input'
-
+import Close from '../../UI/Button/CloseButton/CloseButton'
 
 export default function CreateAccount() {
     return (
         <>
             <div className="CreateAccount-competent max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
+                <Close ClassName={"creat-close"}/>
 
             </div>
         </>
