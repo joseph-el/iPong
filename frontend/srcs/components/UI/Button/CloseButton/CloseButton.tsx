71f9bf3e1b939d12da3d8706ebd4d0a3320f768b
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-10 15:45:46 +0100

    Fix: Add `ClassName` prop to CloseButton
    Adds a `ClassName` prop to the CloseButton component, allowing for custom styling.

--- frontend/srcs/components/UI/Button/CloseButton/CloseButton.tsx
+++ frontend/srcs/components/UI/Button/CloseButton/CloseButton.tsx
@@ -1,9 +1,9 @@
 import React from "react";
 import CloseIcon from './Close.svg'
 
-export default function Close() {
+export default function Close({ClassName}) {
     return (
-        <div className="close">
+        <div className={ClassName}>
             <img className="SF-symbol-xmark" alt="Sf symbol xmark" src={CloseIcon} />
         </div>
     );
