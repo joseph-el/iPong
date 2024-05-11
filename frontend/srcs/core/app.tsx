405e7b09ab89bb31e87ff8da68f2f1431cab5981
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-11 13:06:03 +0100

    Refactor: Update and render auth flow
    Updates the app component to render the entire auth flow, including sign-in, sign-up, and password reset. Also adds a custom CSS file to style the app.

---
+++ frontend/srcs/core/app.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {NextUIProvider} from '@nextui-org/react'
+import './index.css'
+
+import SignInOptions from '../components/AuthComponents/SignInOptions/SignInOptions'
+import SignIn from '../components/AuthComponents/SignIn/SignIn'
+import CreateAccount from '../components/AuthComponents/CreateAccount/CreateAccount'
+import SetUserNameAndPicture from '../components/AuthComponents/SetUserNameAndPicture/SetUserNameAndPicture'
+import WeSentCodeAndPassword from '../components/AuthComponents/WeSentCodeAndPassword/WeSentCodeAndPassword'
+
+export default function App() {
+  return (
+      <>
+              <WeSentCodeAndPassword title={"We sent you a code"} guide_title={"Enter the email associated with your account to change your password."} input_type="Email"  button_text="Next"/>
+              <SetUserNameAndPicture></SetUserNameAndPicture>
+              <CreateAccount/>
+              <SignIn />
+              <SignInOptions />
+      </>
+  );
+}
+
+
+
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
