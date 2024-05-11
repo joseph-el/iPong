c056658d67a3ddaeea030550e043986928863448
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-11 09:29:05 +0100

    Refactor: Update WeSentCodeAndPassword with dynamic props
    - Add props for guide title, input type, and button text.
    - Dynamically display these props in the component.
    - Update close button's class name for styling purposes.

--- frontend/srcs/components/AuthComponents/WeSentCodeAndPassword/WeSentCodeAndPassword.tsx
+++ frontend/srcs/components/AuthComponents/WeSentCodeAndPassword/WeSentCodeAndPassword.tsx
@@ -4,40 +4,35 @@ import CustomButton from '../../UI/Button/SubmitButton/SubmitButton'
 import InputComponent from '../../UI/Input/Input'
 import Close from '../../UI/Button/CloseButton/CloseButton'
 
-export const UserGuide = () => {
+export const UserGuide = ( {guide_title} ) => {
     return (
       <div className="user-name-alert">
         <p className="this-will-not-be-user">
-          Your <span className="user-name">@username</span> is unique. You can
-          always change it later.
+            {guide_title}
         </p>
       </div>
     );
-  };
+};
 
-
-export default function WeSentCodeAndPassword({props}) {
+export default function WeSentCodeAndPassword(props) {
+    
     return (
         <div className="WeSentCodeAndPassword-frame max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
-            <Close ClassName={"close"}/>
-
-
+            <Close ClassName={"close-WeSentCodeAndPassword"}/>
             <div className='WeSentCodeAndPassword'>
-                <div className="text-wrapper-3">Pick a profile picture</div>
-                
-                <UserGuide/>
-                
-                
+
+                <div className="text-wrapper-3">{props.title}</div>
+
+                <UserGuide guide_title={props.guide_title}/>
+
                 <div className="input-padding">
-                    <InputComponent type={"fill"} target={"username"} />
+                    <InputComponent type={"fill"} target={props.input_type} />
                 </div>
                 
                 <div className="buttons-target">
-                    <CustomButton classNames="create-account" text="Next" />
-                 </div>
-
+                    <CustomButton classNames="create-account" text={props.button_text} />
+                </div>
             </div>
-
         </div>
 
     );
