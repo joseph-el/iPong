58a2012b641f8ddc49fc396f83ecef86ba7ab1a4
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-13 11:47:52 +0100

    Refactor: Introduce LoginState enum for Login components

---
+++ frontend/srcs/components/AuthComponents/LoginState/LoginState.ts
@@ -0,0 +1,13 @@
+export enum LoginState {
+    UNK = 0,
+    INVALID_PASSWORD = (1 << 0),
+    INVALID_EMAIL = (1 << 1),
+    LOGIN_SUCCESS = (1 << 2),
+
+    INVALID_NAME = (1 << 3),
+    INVALID_EMAIL_FORMAT = (1 << 4),
+    INVALID_EMAIL_EXISTS = (1 << 5),
+    INVALID_PASSWORD_FORMAT = (1 << 6),
+    INVALID_DATE = (1 << 7),
+    INVALID_DATE_FORMAT = (1 << 8),
+}
