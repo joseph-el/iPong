af4c99cbfd94a73cd7decc2ba1b679b87f889fe2
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-13 20:11:32 +0100

    feat(store): add gender slice to store

--- frontend/srcs/state/store.ts
+++ frontend/srcs/state/store.ts
@@ -1,13 +1,15 @@
 import { ReducerType, configureStore } from '@reduxjs/toolkit'
 import inputReducer from "./InputComponents/inputSlice"
 import dateSlice from "./InputComponents/InputDateComponent/dateSlice"
+import genderSlice from './Checkbox/CheckboxSlice';
 
 export const store = configureStore({
     reducer: {
       input: inputReducer,
       date: dateSlice,
+      gender: genderSlice
     },
-  });
+});
 
 
 export type AppDispatch = typeof store.dispatch
