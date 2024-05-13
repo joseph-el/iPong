6125ab870bdbee28a7c91f28c256ea1e9beb538b
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-13 20:10:50 +0100

    Fix: Enable CORS for backend

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,7 +5,12 @@ import { ValidationPipe } from '@nestjs/common';
 import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: ["http://localhost:5173"],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+});
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   const config = new DocumentBuilder()
