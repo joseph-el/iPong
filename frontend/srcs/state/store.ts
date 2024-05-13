e598970d254a29eab94f9fff351dfe0ff609d23d
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-13 21:50:33 +0100

    feat: add user state slice
    Adds a new slice to the store for managing user state.

--- frontend/srcs/state/store.ts
+++ frontend/srcs/state/store.ts
@@ -2,12 +2,13 @@ import { ReducerType, configureStore } from '@reduxjs/toolkit'
 import inputReducer from "./InputComponents/inputSlice"
 import dateSlice from "./InputComponents/InputDateComponent/dateSlice"
 import genderSlice from './Checkbox/CheckboxSlice';
-
+import UserSlice from './UserInfo/UserSlice';
 export const store = configureStore({
     reducer: {
       input: inputReducer,
       date: dateSlice,
-      gender: genderSlice
+      gender: genderSlice,
+      userState: UserSlice,
     },
 });
 
