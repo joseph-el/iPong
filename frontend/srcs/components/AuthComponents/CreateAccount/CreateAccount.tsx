c821e19ab7f9e2e1325c0d955d617f6dcd8b8d48
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-13 11:47:15 +0100

    feat: Integrate Redux to handle input validation and states
    - Adds Redux state management to the CreateAccount component.
    - Implements input validation using the Redux state.
    - Handles form submission and validation errors.
    - Sets error messages, disables buttons, and updates values in the Redux store.

--- frontend/srcs/components/AuthComponents/CreateAccount/CreateAccount.tsx
+++ frontend/srcs/components/AuthComponents/CreateAccount/CreateAccount.tsx
@@ -5,6 +5,17 @@ import CustomCheckbox from '../../UI/Checkbox/Checkbox'
 import Close from '../../UI/Button/CloseButton/CloseButton'
 import CustomButton from '../../UI/Button/SubmitButton/SubmitButton'
 import CustomDateInput from '../../UI/Input/DateInput/CustomDateInput'
+import { LoginState } from '../LoginState/LoginState'
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../state/store";
+import InputState from '../../../state/InputComponents/inputSlice'
+import {
+  setColor,
+  setIsInvalid,
+  setErrorMessage,
+  setValue,
+} from "../../../state/InputComponents/inputSlice";
+import { useNavigate } from "react-router-dom";
 
 export const DateOfBirth = () => {
 
@@ -20,7 +31,21 @@ export const DateOfBirth = () => {
 
 };
 
+
 export default function CreateAccount() {
+    
+    const fullname = useSelector((state: RootState) => state.input["creat-accout-full-nam"]?.value);
+    const password = useSelector((state: RootState) => state.input["creat-accout-email"]?.value);
+
+  
+    const handelonSubmit = () => {
+        var state: LoginState = LoginState.UNK;
+    
+    
+    
+    
+    }
+    
     return (
         <>
             <div className="CreateAccount-competent-frame max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
@@ -30,8 +55,8 @@ export default function CreateAccount() {
                     
                     <div className="text-wrapper-3">Create an account</div>
                  
-                        <InputComponent type={"fill"} target={"Full Name"}/>
-                        <InputComponent type={"fill"} target={"Address Email"}/>
+                        <InputComponent type={"fill"} target={"Full Name"} id="creat-accout-full-name" placeholder="Enter your full name"/>
+                        <InputComponent type={"fill"} target={"Address Email"} id="creat-accout-email" placeholder="Enter your full email" />
 
                         <DateOfBirth/>
                         <CustomDateInput/>
@@ -42,7 +67,7 @@ export default function CreateAccount() {
 
                         <CustomCheckbox/>
 
-                        <div className='buttons-target'>
+                        <div className='buttons-target' onClick={handelonSubmit}>
                             <CustomButton
                                 classNames="create-account"
                                 text="Next"
