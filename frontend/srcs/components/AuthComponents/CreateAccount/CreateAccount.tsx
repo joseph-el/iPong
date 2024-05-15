df9074df9d3f6ae8f9259e8ff8f858cf9893ed41
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-15 12:41:31 +0100

    Fix: Update Create Account UI and handle close button
    Updates the Create Account UI to fix layout issues and adds functionality to the close button.

--- frontend/srcs/components/AuthComponents/CreateAccount/CreateAccount.tsx
+++ frontend/srcs/components/AuthComponents/CreateAccount/CreateAccount.tsx
@@ -5,20 +5,15 @@ import CustomCheckbox from '../../UI/Checkbox/Checkbox'
 import Close from '../../UI/Button/CloseButton/CloseButton'
 import CustomButton from '../../UI/Button/SubmitButton/SubmitButton'
 import CustomDateInput from '../../UI/Input/DateInput/CustomDateInput'
-import { LoginState } from '../LoginState/LoginState'
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../state/store";
-import InputState from '../../../state/InputComponents/inputSlice'
 import {
-  setColor,
   setIsInvalid,
   setErrorMessage,
-  setValue,
 } from "../../../state/InputComponents/inputSlice";
 import {
     setDateIsInvalid,
     setDateErrorMessage,
-    setDateValue,
   } from "../../../state/InputComponents/InputDateComponent/dateSlice";
 
 import {
@@ -34,8 +29,7 @@ export const DateOfBirth = () => {
     return (
         <div className="date-of-birth">
             <p className="this-will-not-be">
-                This will not be shown publicly. Confirm your own age, even if this <br />
-                account is for a business, a pet, or something else.
+                This will not be shown publicly. Confirm your own age, a pet, or something else.
             </p>
             <div className="text-wrapper">Date of birth</div>
         </div>
@@ -57,17 +51,18 @@ export default function CreateAccount() {
         const errors = await getValidationErrors();
 
         if (!errors)  {
-            console.log("Fullname: ", fullname);
-            console.log("Email: ", email);
-            console.log("Date of birth: ", date_of_birth);
-            console.log("UserGender: ", UserGender);
-            
             navigate("/Login/need-a-password");
         }
          else 
             handleErrors(errors);
     };
 
+    const handelonClose = () => {
+        // TDOD: Close the sign in component and navigate to the sign up component
+        // Clear the input fields state and error messages
+        navigate("/login");
+        console.log("Close");
+    }
    
     const getValidationErrors =  async () => {
         if (!fullname ) {
@@ -75,22 +70,20 @@ export default function CreateAccount() {
         } else if (!isFullNameValid(fullname)) {
             return { id: 'create-account-full-name', message: 'Invalid name format' };
         }
-    
         if (!email) {
             return { id: 'create-account-email', message: 'Email required!' };
         }
-
         const emailError =  await validateEmail(email)
-
         if (typeof emailError === 'string') {
             return { id: 'create-account-email', message: emailError };
         }
-        if (!date_of_birth) 
+        if (!date_of_birth) {
             return { id: 'undefined', message: 'Date of birth required!' };
-        
-        // } else if (!isDateOfBirthValid(date_of_birth)) {
-        //     return { id: 'undefined', message: 'Invalid date, should be after 2005' };
-        // }
+        }
+        const validationResult = isDateOfBirthValid(date_of_birth);
+        if (validationResult !== null) {
+            return { id: 'undefined', message: validationResult };
+        }
         if (UserGender == GenderType.UNK) {
             return { id: 'undefined_gender', message: 'User gender required!'};
         }
@@ -118,7 +111,9 @@ export default function CreateAccount() {
     return (
         <>
             <div className="CreateAccount-competent-frame max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
-                <Close ClassName={"creat-close"}/>
+                
+                
+                <Close ClassName={"creat-close"} id={"close"} func={handelonClose}  />
 
                 <div className='CreateAccount-competent'>
                     
@@ -128,13 +123,18 @@ export default function CreateAccount() {
                         <InputComponent type={"fill"} target={"Address Email"} id="create-account-email" placeholder="Enter your full email" />
 
                         <DateOfBirth/>
-                        <CustomDateInput/>
+                        
+                        <div className="date-of-birth-resize">
+                            <CustomDateInput/>
+                        </div>
 
                         <div className="gender">
                             <div className="text-wrapper-4">Gender</div>
                         </div>
-
-                        <CustomCheckbox/>
+                        
+                        <div className='move-gender'>
+                            <CustomCheckbox/>
+                        </div>
 
                         <div className='buttons-target' onClick={handleOnSubmit}>
                             <CustomButton
