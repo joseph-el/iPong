d5f02dda396e084ff644e32c19c82ee93a297136
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-15 12:42:16 +0100

    Fix: Use custom CSS for CloseButton and add BackIcon
    Modifies CloseButton component by using custom CSS and adding a BackIcon that can be used to navigate back when clicked.

--- frontend/srcs/components/UI/Button/CloseButton/CloseButton.tsx
+++ frontend/srcs/components/UI/Button/CloseButton/CloseButton.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import CloseIcon from './Close.svg'
+import './CloseButton.css'
+import BackIcon from './Back.svg'
 
-export default function Close({ClassName}) {
+export default function Close({ClassName, func, id}) {
     return (
-        <div className={ClassName}>
-            <img className="SF-symbol-xmark" alt="Sf symbol xmark" src={CloseIcon} />
+        <div className={ClassName} >
+            <img onClick={func} className="SF-symbol-xmark" alt="Sf symbol xmark" src={ id == "close"? CloseIcon: BackIcon} />
         </div>
     );
 };
