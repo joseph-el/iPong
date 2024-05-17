36a8a25130d8e4e2162ca8ed378a4c153daf180b
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-17 21:06:36 +0100

    feat(frontend): Add Live Chat component

---
+++ frontend/srcs/components/LiveChat/LiveChat.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./LiveChat.css";
+export default function LiveChat() {
+    return (
+        <div className="live-chat">
+            <span > L </span>
+        </div>
+    );
+};
