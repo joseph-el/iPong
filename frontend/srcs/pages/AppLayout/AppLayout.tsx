db556a6421dee70cbf578a859c2694e1aa9b8cd5
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-17 21:06:17 +0100

    Refactor: Remove unused SearchInput and add LiveChat component to AppLayout
    - Removed unused SearchInput component.
    - Added LiveChat component to the AppLayout.
    - Updated grid height and sidebar background color.

--- frontend/srcs/pages/AppLayout/AppLayout.tsx
+++ frontend/srcs/pages/AppLayout/AppLayout.tsx
@@ -4,26 +4,10 @@ import { SideBar } from '../../components/SideBar/SideBar';
 import {Divider} from "@nextui-org/divider";
 import NavBar from '../../components/NavBar/NavBar';
 import './AppLayout.css';
-import SearchInput from '../../components/UI/SearchInput/SearchInput';
+// import SearchInput from '../../components/UI/SearchInput/SearchInput';
 
-export function AiCOn() {
-  return (
-    <div className="max-w-md">
-      <div className="space-y-1">
-        <h4 className="text-medium font-medium">NextUI Components</h4>
-        <p className="text-small text-default-400">Beautiful, fast and modern React UI library.</p>
-      </div>
-      <Divider className="my-4" />
-      <div className="flex h-5 items-center space-x-4 text-small">
-        <div>Blog</div>
-        <Divider orientation="horizontal" />
-        <div>Docs</div>
-        <Divider orientation="horizontal" />
-        <div>Source</div>
-      </div>
-    </div>
-  );
-}
+// acczss_token
+import LiveChat from '../../components/LiveChat/LiveChat';
 
 
 
@@ -35,14 +19,14 @@ export default function AppLayout() {
         "sidebar main livechat"
         "sidebar main livechat"
       `}
-      gridTemplateRows={'70px 1fr 30px'}
+      gridTemplateRows={'72px 1fr 30px'}
       gridTemplateColumns={'78px 1fr 200px'} // Specify the widths for sidebar and live chat
       h='100vh' // Set the height of the Grid container to 100vh
       gap='0'
-      color='blackAlpha.700'
+      color='white'
       fontWeight='bold'
     >
-      <GridItem pl='2' area={'nav'}>
+      <GridItem pl='2'  area={'nav'} rowSpan={1}>
         <NavBar/>
       </GridItem>
 
@@ -50,12 +34,12 @@ export default function AppLayout() {
         <SideBar />
       </GridItem>
 
-      <GridItem pl='2' bg='GrayText' area={'main'}>
+      <GridItem pl='2' area={'main'} w="full" h="full">
         main content
       </GridItem>
 
       <GridItem pl='2' bg='black' area={'livechat'}>
-        Live Chat
+        <LiveChat />
       </GridItem>
     </Grid>
   );
