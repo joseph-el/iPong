c70170453fd40b8aceaacf1cdbce8b6c3afbe841
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-18 20:01:34 +0100

    Add Logout component to Home page

--- frontend/srcs/pages/Home/Home.tsx
+++ frontend/srcs/pages/Home/Home.tsx
@@ -1,3 +1,11 @@
 import React from 'react'
 import './Home.css'
 
+import Logout from '../../components/UI/Logout/Logout';
+export default function Home() {
+    return (
+        <div className="Home">
+                <Logout />
+        </div>
+    );
+};
