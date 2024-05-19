3b0f1b9d26d926f157a4d978069cddc0fa5db719
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-19 18:27:51 +0100

    feat: Implement Notifications Bar
    Adds a notifications bar to the frontend.

---
+++ frontend/srcs/components/UI/NotificationsBar/NotificationsBar.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import './NotificationsBar.css';
+import { NotificationsWrapper } from "./NotificationsWrapper";
+
+export default function NotificationsBar() {
+    return (
+        <div className="NotificationsBar-frame">
+            <NotificationsWrapper>
+
+                <h1> hello</h1>
+
+
+            </NotificationsWrapper>
+
+
+        </div>
+    );
+}
