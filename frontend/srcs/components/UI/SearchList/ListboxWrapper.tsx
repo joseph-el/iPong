981f8648e18df8d34d6abd28e95c00c5f6f9ad6d
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-19 14:34:15 +0100

    feat: Update SearchList.ListboxWrapper component styles

--- frontend/srcs/components/UI/SearchList/ListboxWrapper.tsx
+++ frontend/srcs/components/UI/SearchList/ListboxWrapper.tsx
@@ -1,7 +1,13 @@
 import React from "react";
+import './SearchList.css'
+
 
 export const ListboxWrapper = ({children}: { children: React.ReactNode }) => (
-  <div className="w-full max-w-[335px] border-small px-1 py-2 rounded-small bg-black border-default-200 ">
-    {children}
+  <div className="list-search-box">
+
+
+    <div className="w-full w-48 sm:w-48 md:w-64 lg:w-64 xl:w-72 2xl:w-80          border-small px-1 py-2 rounded-small bg-black  ">
+      {children}
+    </div>
   </div>
 );
