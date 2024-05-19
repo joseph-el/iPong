dc66e2700a7e431146b0bd4e478af1a25de34ba7
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-19 14:34:42 +0100

    Fix: Remove Notification Center from Home page

--- frontend/srcs/pages/Home/Home.tsx
+++ frontend/srcs/pages/Home/Home.tsx
@@ -2,10 +2,15 @@ import React from 'react'
 import './Home.css'
 
 import Logout from '../../components/UI/Logout/Logout';
+
+
+
+
 export default function Home() {
     return (
         <div className="Home">
-                <Logout />
+            {/* <NotificationCenter/> */}
+                {/* <Logout /> */}
         </div>
     );
 };
