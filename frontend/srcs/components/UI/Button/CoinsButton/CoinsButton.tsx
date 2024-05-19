5955b4a0d18106edd988655d3d0e46a1f68654fa
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-19 14:33:43 +0100

    Fix: Adjust v-bucks icon size and add css class
    Fixes the v-bucks icon size and adds a CSS class to the CoinsButton component.

--- frontend/srcs/components/UI/Button/CoinsButton/CoinsButton.tsx
+++ frontend/srcs/components/UI/Button/CoinsButton/CoinsButton.tsx
@@ -4,9 +4,9 @@ import CoinsIcon from './coins.svg'
 
 export default function CoinsButton(props) {
     return (
-        <div className="flex items-center justify-center">
+        <div className="flex items-center justify-center  coins-button">
             <div className="price bg-white rounded-md flex items-center">
-                <img className="v-bucks-removebg w-6 h-6 object-cover mr-1" alt="V bucks removebg" src={CoinsIcon}/>
+                <img className="v-bucks-removebg w-[20px] h-[20px] object-cover mr-1" alt="V bucks removebg" src={CoinsIcon}/>
                 <div className="text-wrapper text-black font-semibold text-lg flex-grow">
                     
                     {props.coins.length > 4 ? props.coins.substring(0, 4) + '...' : props.coins}
