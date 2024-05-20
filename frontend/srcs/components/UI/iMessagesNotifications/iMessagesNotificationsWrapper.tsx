61c12abf4e4769a801387a999705de0268a437aa
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-20 11:46:25 +0100

    Refactor: Remove fixed width from iMessages wrapper
    Removes the fixed width from the iMessages wrapper.

--- frontend/srcs/components/UI/iMessagesNotifications/iMessagesNotificationsWrapper.tsx
+++ frontend/srcs/components/UI/iMessagesNotifications/iMessagesNotificationsWrapper.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import './iMessagesNotifications.css';
+
 export const ImessagesNotificationsWrapper = ({children}) => (
-  <div className="w-full w-[350px] wrapper-style  px-1 py-2 ">
+  <div className="wrapper-style">
     {children}
   </div>
 );
