5f05c30d9d46510f0f0c4ec9c20608d40c4f8ce6
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-20 11:46:21 +0100

    Fix: Display iMessage notifications
    Changes in iMessagesNotifications.tsx:
    - Added user avatar, message description, and iMessage icon to notification

--- frontend/srcs/components/UI/iMessagesNotifications/iMessagesNotifications.tsx
+++ frontend/srcs/components/UI/iMessagesNotifications/iMessagesNotifications.tsx
@@ -4,22 +4,24 @@ import { ImessagesNotificationsWrapper } from "./iMessagesNotificationsWrapper";
 import ImessageIcon from './imessage.svg'
 import {User} from "@nextui-org/react";
 
+
+
 export default function ImessagesNotifications() {
     return (
         <ImessagesNotificationsWrapper>
-        
-
-            <div className="time-of-notification">
-            15m ago
+            <div className="notification-content">
+                <User   
+                    name="iMessages"
+                    description="Taha Naceur send you a message"
+                    avatarProps={{
+                        src: "https://cdn.intra.42.fr/users/1e212df3b450650d04418d1c03827563/tnaceur.jpg"
+                    }}
+                />
+                <div className="time-of-notification">
+                    15m ago
+                </div>
+                <img src={ImessageIcon} alt="iMessage" className="imessage-icon"/>
             </div>
-        <User   
-            name="iMessages"
-            description="Taha Naceur send you a message"
-            avatarProps={{
-                src: "https://cdn.intra.42.fr/users/1e212df3b450650d04418d1c03827563/tnaceur.jpg"
-            }}
-    
-        />
         </ImessagesNotificationsWrapper>
-        );
+    );
 }
