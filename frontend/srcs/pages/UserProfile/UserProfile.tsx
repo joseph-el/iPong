0f5167b54168e2ddbb1d561c1b9d8b44bea0bc48
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-21 23:19:36 +0100

    feat: Add UserProfile page
    Add a new page to show a user profile.

---
+++ frontend/srcs/pages/UserProfile/UserProfile.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './UserProfile.css';
+
+export default function UserProfile(props) {
+
+
+    return (
+        <>
+            <div className='UserProfile-frame'>
+                User
+            </div>
+        </>
+    );
+
+
+}
