e4fa947c7ea7a009bf0a3985146e9988150013d7
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-21 23:19:28 +0100

    Fix: Adjust SearchList width for mobile view

--- frontend/srcs/components/UI/SearchList/ListboxWrapper.tsx
+++ frontend/srcs/components/UI/SearchList/ListboxWrapper.tsx
@@ -6,7 +6,7 @@ export const ListboxWrapper = ({children}: { children: React.ReactNode }) => (
   <div className="list-search-box">
 
 
-    <div className="w-full w-48 sm:w-48 md:w-64 lg:w-64 xl:w-72 2xl:w-80          border-small px-1 py-2 rounded-small bg-black  ">
+    <div className="w-full  w-[226px] sm:w-48  md:w-64 lg:w-64 xl:w-72 2xl:w-80          border-small px-1 py-2 rounded-small bg-black  ">
       {children}
     </div>
   </div>
