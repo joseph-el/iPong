b52bed2dba5193f8b9aa55e6a306c4aac400a5bc
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-21 09:16:03 +0100

    Refactor: Pass name and avatar props to iMessagesNotifications
    Changes the iMessagesNotifications component to accept name and avatar props, allowing for dynamic display of the sender's information.

--- frontend/srcs/components/UI/iMessagesNotifications/iMessagesNotifications.tsx
+++ frontend/srcs/components/UI/iMessagesNotifications/iMessagesNotifications.tsx
@@ -4,23 +4,23 @@ import { ImessagesNotificationsWrapper } from "./iMessagesNotificationsWrapper";
 import ImessageIcon from './imessage.svg'
 import {User} from "@nextui-org/react";
 
-
-
-export default function ImessagesNotifications() {
+export default function ImessagesNotifications(props) {
     return (
         <ImessagesNotificationsWrapper>
             <div className="notification-content">
                 <User   
                     name="iMessages"
-                    description="Taha Naceur send you a message"
+                    description={props.name +  " send you a message"}
                     avatarProps={{
-                        src: "https://cdn.intra.42.fr/users/1e212df3b450650d04418d1c03827563/tnaceur.jpg"
+                        src: props.avatar
                     }}
                 />
                 <div className="time-of-notification">
-                    15m ago
+                    {props.time}
                 </div>
-                <img src={ImessageIcon} alt="iMessage" className="imessage-icon"/>
+                {
+                    /* <img src={ImessageIcon} alt="iMessage" className="imessage-icon"/> */ 
+                }
             </div>
         </ImessagesNotificationsWrapper>
     );
