867bc52592078f0fd54f44a1461e70f8ff221f62
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-26 18:45:06 +0100

    Update user photo for Logout component

--- frontend/srcs/components/UI/Logout/Logout.tsx
+++ frontend/srcs/components/UI/Logout/Logout.tsx
@@ -19,7 +19,7 @@ export default function Logout(props) {
 
         <Avatar
             className="User-avatar"
-            src="https://cdn.intra.42.fr/users/1e212df3b450650d04418d1c03827563/tnaceur.jpg"
+            src="https://scontent.frba4-1.fna.fbcdn.net/v/t39.30808-1/417474877_1084959666153312_6596618040732418232_n.jpg?stp=dst-jpg_p480x480&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHwTc_R7JPrAPtBWxFRaFe-erb5zxyu1hd6tvnPHK7WF9S_wr1S8zpCfu2aMDYk7-iTba5XwscZ6PA2aqXg6Q-h&_nc_ohc=r3Gu5KyihjkQ7kNvgGYZlZx&_nc_ht=scontent.frba4-1.fna&oh=00_AYBJlWqZwMOPo4HJqEk88jKQPnd68pOIPERWEytk02mbZw&oe=6658F8C7"
             size="lg"
       />
 
