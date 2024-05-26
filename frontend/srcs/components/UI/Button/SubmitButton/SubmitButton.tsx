2012c424d99406e723faa9cb6c858e8bf4036cb1
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-26 18:45:01 +0100

    feat: Add onClick prop to CustomButton
    Adds an onClick prop to CustomButton, allowing it to be used as a submit button.

--- frontend/srcs/components/UI/Button/SubmitButton/SubmitButton.tsx
+++ frontend/srcs/components/UI/Button/SubmitButton/SubmitButton.tsx
@@ -2,9 +2,9 @@ import PropTypes from "prop-types";
 import React from "react";
 import "./SubmitButton.css";
 
-export default function CustomButton ({ text, classNames  }) {
+export default function CustomButton ({ text, classNames, onClick  }) {
     return (
-        <div className={classNames}>
+        <div className={classNames} onClick={onClick}>
             <div className="group">
                 <div className="div-wrapper">
                     <div className="text-wrapper">{text}</div>
