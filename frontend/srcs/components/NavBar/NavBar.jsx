e3efd59f611afbb35531ccee663a28531e576bf7
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-26 18:44:56 +0100

    Refactor: Update NavBar user avatar

--- frontend/srcs/components/NavBar/NavBar.jsx
+++ frontend/srcs/components/NavBar/NavBar.jsx
@@ -125,7 +125,7 @@ export default function NavBar() {
 
                         <NavBarUser fullName="Taha Naceur" email="tahanaceur48@icloud.ma" username="tnaceur"
                             onClick={() => {}}
-                            avatar="https://cdn.intra.42.fr/users/1e212df3b450650d04418d1c03827563/tnaceur.jpg" />
+                            avatar="https://scontent.frba4-1.fna.fbcdn.net/v/t39.30808-1/417474877_1084959666153312_6596618040732418232_n.jpg?stp=dst-jpg_p480x480&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHwTc_R7JPrAPtBWxFRaFe-erb5zxyu1hd6tvnPHK7WF9S_wr1S8zpCfu2aMDYk7-iTba5XwscZ6PA2aqXg6Q-h&_nc_ohc=r3Gu5KyihjkQ7kNvgGYZlZx&_nc_ht=scontent.frba4-1.fna&oh=00_AYBJlWqZwMOPo4HJqEk88jKQPnd68pOIPERWEytk02mbZw&oe=6658F8C7" />
                     
                     
                     </div> : null
