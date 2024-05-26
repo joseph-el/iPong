3e4b0918b9d3fe50ad5ce039fd6090f86368be7d
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-26 18:45:33 +0100

    Refactor: Introduce EditSecurity wrapper component

---
+++ frontend/srcs/components/UI/ProfileSettings/EditSecurity/EditSecurityWrapper.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './EditSecurity.css';
+
+export const EditSecurityWrapper = ({children}) => (
+  <div className="EditProfileWrapper-box">
+    {children}
+  </div>
+);
