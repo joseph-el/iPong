55e8dfa824c8a803c90ae779bf8e5f03ff2d6986
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-26 18:45:17 +0100

    Fix: Correct LinkedIn label spelling in Edit Profile page

--- frontend/srcs/components/UI/ProfileSettings/EditProfile/EditProfile.tsx
+++ frontend/srcs/components/UI/ProfileSettings/EditProfile/EditProfile.tsx
@@ -24,9 +24,6 @@ const EditProfileNavbar = (props) => {
   };
 
 
-
-
-  
 export default function EditProfile(props) {
 
     const InputTypes = [
@@ -53,7 +50,7 @@ export default function EditProfile(props) {
         {
             type: "text",
             placeholder: "https://www.linkedin.com/in/youssef-el-idrissi-1b1b1b1b1",
-            label: "LinkenIn:\f\f\f",
+            label: "Linkedin:\f\f\f",
         },
         {
             type: "text",
@@ -72,7 +69,7 @@ export default function EditProfile(props) {
                     src={CoverImage}
                 />
                 <Avatar
-                    src="https://cdn.intra.42.fr/users/1e212df3b450650d04418d1c03827563/tnaceur.jpg"
+                    src="https://scontent.frba4-1.fna.fbcdn.net/v/t39.30808-1/417474877_1084959666153312_6596618040732418232_n.jpg?stp=dst-jpg_p480x480&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_eui2=AeHwTc_R7JPrAPtBWxFRaFe-erb5zxyu1hd6tvnPHK7WF9S_wr1S8zpCfu2aMDYk7-iTba5XwscZ6PA2aqXg6Q-h&_nc_ohc=r3Gu5KyihjkQ7kNvgGYZlZx&_nc_ht=scontent.frba4-1.fna&oh=00_AYBJlWqZwMOPo4HJqEk88jKQPnd68pOIPERWEytk02mbZw&oe=6658F8C7"
                     className="w-24 h-24 text-large avatar-edit-profile"
                 />
                 <div className="edit-avatar-edit-profile">
