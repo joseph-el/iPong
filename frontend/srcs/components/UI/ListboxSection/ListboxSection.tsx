7321059ad279c5846bc6eb5760509d2ae7e8f196
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:51:16 +0100

    Refactor: Updated spacing in ListboxWrapper

--- frontend/srcs/components/UI/ListboxSection/ListboxSection.tsx
+++ frontend/srcs/components/UI/ListboxSection/ListboxSection.tsx
@@ -1,18 +1,19 @@
 import React from "react";
-import {Listbox, ListboxItem, ListboxSection, cn} from "@nextui-org/react";
-import {ListboxWrapper} from "./ListboxWrapper";
-import {AddNoteIcon} from "./AddNoteIcon.tsx";
-import {CopyDocumentIcon} from "./CopyDocumentIcon.tsx";
-import {EditDocumentIcon} from "./EditDocumentIcon.tsx";
-import {DeleteDocumentIcon} from "./DeleteDocumentIcon.tsx";
+import { Listbox, ListboxItem, ListboxSection, cn } from "@nextui-org/react";
+import { ListboxWrapper } from "./ListboxWrapper";
+import { AddNoteIcon } from "./AddNoteIcon.tsx";
+import { CopyDocumentIcon } from "./CopyDocumentIcon.tsx";
+import { EditDocumentIcon } from "./EditDocumentIcon.tsx";
+import { DeleteDocumentIcon } from "./DeleteDocumentIcon.tsx";
 
 export default function SearchListboxSection() {
-  const iconClasses = "text-xl text-default-500 pointer-events-none flex-shrink-0";
+  const iconClasses =
+    "text-xl text-default-500 pointer-events-none flex-shrink-0";
 
   return (
     <ListboxWrapper>
       <Listbox variant="flat" aria-label="Listbox menu with sections">
-        <ListboxSection title="Users" showDivider>  
+        <ListboxSection title="Users" showDivider>
           <ListboxItem
             key="new"
             description="Create a new file"
@@ -34,18 +35,20 @@ export default function SearchListboxSection() {
           >
             Edit file
           </ListboxItem>
-        </ListboxSection> 
-        <ListboxSection title="Settings">  
+        </ListboxSection>
+        <ListboxSection title="Settings">
           <ListboxItem
             key="delete"
             className="text-danger"
             color="danger"
             description="Permanently delete the file"
-            startContent={<DeleteDocumentIcon className={cn(iconClasses, "text-danger")} />}
+            startContent={
+              <DeleteDocumentIcon className={cn(iconClasses, "text-danger")} />
+            }
           >
             Delete file
           </ListboxItem>
-        </ListboxSection> 
+        </ListboxSection>
       </Listbox>
     </ListboxWrapper>
   );
