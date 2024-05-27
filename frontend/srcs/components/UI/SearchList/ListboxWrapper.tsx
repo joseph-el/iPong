7d5f26d1aa26f5683a4fbd8e83f168f173e6bd67
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:52:53 +0100

    Fix(SearchList): Update ListboxWrapper CSS class name
    Fixes styling issue in ListboxWrapper component by updating the CSS class name.

--- frontend/srcs/components/UI/SearchList/ListboxWrapper.tsx
+++ frontend/srcs/components/UI/SearchList/ListboxWrapper.tsx
@@ -1,11 +1,8 @@
 import React from "react";
-import './SearchList.css'
+import "./SearchList.css";
 
-
-export const ListboxWrapper = ({children}: { children: React.ReactNode }) => (
+export const ListboxWrapper = ({ children }: { children: React.ReactNode }) => (
   <div className="list-search-box">
-
-
     <div className="w-full  w-[226px] sm:w-48  md:w-64 lg:w-64 xl:w-72 2xl:w-80          border-small px-1 py-2 rounded-small bg-black  ">
       {children}
     </div>
