42173d0f380a2ee1001bfa61249dca5d0d4b3bd9
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:49:58 +0100

    Refactor: Updated component implementation for Checkbox

--- frontend/srcs/components/UI/Checkbox/Checkbox.tsx
+++ frontend/srcs/components/UI/Checkbox/Checkbox.tsx
@@ -1,50 +1,53 @@
 import React from "react";
-import {Checkbox} from "@nextui-org/react";
-import './Checkbox.css'
+import { Checkbox } from "@nextui-org/react";
+import "./Checkbox.css";
 
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../state/store";
 
-import {GenderType} from "../../../state/Checkbox/CheckboxSlice";
+import { GenderType } from "../../../state/Checkbox/CheckboxSlice";
 import {
   setGenderInvalide,
   setGender,
- 
 } from "../../../state/Checkbox/CheckboxSlice";
 
 export default function CustomCheckbox() {
+  const gender_type = useSelector(
+    (state: RootState) => state.gender?.UserGender
+  );
+  const is_invalide = useSelector((state: RootState) => state.gender?.Invalide);
 
-
-const gender_type  = useSelector((state: RootState) => state.gender?.UserGender);
-const is_invalide  = useSelector((state: RootState) => state.gender?.Invalide);
-
-
-const dispatch = useDispatch<AppDispatch>();
-const handleMaleChange = () => {
+  const dispatch = useDispatch<AppDispatch>();
+  const handleMaleChange = () => {
     dispatch(setGenderInvalide(false));
     dispatch(setGender(GenderType.MALE));
-};
+  };
 
-const handleFemaleChange = () => {
-  dispatch(setGenderInvalide(false));
+  const handleFemaleChange = () => {
+    dispatch(setGenderInvalide(false));
     dispatch(setGender(GenderType.FELMALE));
-};
-  
+  };
+
   return (
     <>
-        <div className="flex gap-4">
-            <Checkbox className="Gender-type"  isSelected={gender_type == GenderType.MALE} onChange={handleMaleChange}>male</Checkbox>
-            <Checkbox className="Gender-type"  isSelected={gender_type == GenderType.FELMALE} onChange={handleFemaleChange}>female</Checkbox>
-        </div>
+      <div className="flex gap-4">
+        <Checkbox
+          className="Gender-type"
+          isSelected={gender_type == GenderType.MALE}
+          onChange={handleMaleChange}
+        >
+          male
+        </Checkbox>
+        <Checkbox
+          className="Gender-type"
+          isSelected={gender_type == GenderType.FELMALE}
+          onChange={handleFemaleChange}
+        >
+          female
+        </Checkbox>
+      </div>
 
-        {is_invalide && (
-            <p className="text-default-500">
-              User gender required!
-            </p>
-        )}
-
-    
+      {is_invalide && <p className="text-default-500">User gender required!</p>}
     </>
-    
   );
 }
