6487c7fb0ad9071e22fb9331441d09220375a044
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:53:01 +0100

    feat: Refactor SearchList component styles
    Rewrote SearchList component to improve styling with NextUI.

--- frontend/srcs/components/UI/SearchList/SearchList.tsx
+++ frontend/srcs/components/UI/SearchList/SearchList.tsx
@@ -1,11 +1,17 @@
 import React from "react";
 import { useState } from "react";
-import {Listbox, ListboxItem, Chip, ScrollShadow, Avatar, Selection} from "@nextui-org/react";
-import {ListboxWrapper} from "./ListboxWrapper";
-import {users} from "./data";
+import {
+  Listbox,
+  ListboxItem,
+  Chip,
+  ScrollShadow,
+  Avatar,
+  Selection,
+} from "@nextui-org/react";
+import { ListboxWrapper } from "./ListboxWrapper";
+import { users } from "./data";
 
-export default function SearchList({users}) {
-
+export default function SearchList({ users }) {
   const [values, setValues] = useState<Selection>(new Set(["1"]));
 
   const arrayValues = Array.from(values);
@@ -21,9 +27,9 @@ export default function SearchList({users}) {
         className="w-full flex py-0.5 px-2 gap-1"
         orientation="horizontal"
       >
-      <div className="search-title">
-        <Chip > Users </Chip>
-      </div>
+        <div className="search-title">
+          <Chip> Users </Chip>
+        </div>
       </ScrollShadow>
     );
   }, [arrayValues.length]);
@@ -31,7 +37,6 @@ export default function SearchList({users}) {
   return (
     <div className="list-search-box-items">
       <ListboxWrapper>
-        
         <Listbox
           topContent={topContent}
           classNames={{
@@ -44,19 +49,28 @@ export default function SearchList({users}) {
           variant="flat"
         >
           {(item) => (
-            <ListboxItem key={item.id} textValue={item.name} onClick={() => {
-              console.log("user_selected: ", item.name)
-            }}>  
+            <ListboxItem
+              key={item.id}
+              textValue={item.name}
+              onClick={() => {
+                console.log("user_selected: ", item.name);
+              }}
+            >
               <div className="flex gap-2 items-center">
-                <Avatar alt={item.name} className="flex-shrink-0" size="md" src={item.avatar} />
+                <Avatar
+                  alt={item.name}
+                  className="flex-shrink-0"
+                  size="md"
+                  src={item.avatar}
+                />
                 <div className="flex flex-col">
                   <span className="text-small">{item.name}</span>
-                  <span className="text-tiny text-default-400">{item.email}</span>
+                  <span className="text-tiny text-default-400">
+                    {item.email}
+                  </span>
                 </div>
               </div>
-
             </ListboxItem>
-            
           )}
         </Listbox>
       </ListboxWrapper>
