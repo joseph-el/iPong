b54875e2350905bf586f9da8f639277704c7b06f
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:52:45 +0100

    feat: Update UI for 2FA success and next button
    - Revise layout of successful setup screen
    - Update button styling and functionality for "Next" button

--- frontend/srcs/components/UI/ProfileSettings/TwoFactorAuthenticationProfile/TwoFactorAuthenticationProfile.tsx
+++ frontend/srcs/components/UI/ProfileSettings/TwoFactorAuthenticationProfile/TwoFactorAuthenticationProfile.tsx
@@ -5,7 +5,7 @@ import LeftIcon from "./lefticon.svg";
 import { Link, Switch, cn, Input, Image } from "@nextui-org/react";
 import QRtest from "./QrcodeTest.svg";
 import CustomButton from "../../Button/SubmitButton/SubmitButton";
-import Icon2FASuccess from './2FA.svg'
+import Icon2FASuccess from "./2FA.svg";
 
 const TwoFactorAuthenticationProfileNavbar = (props) => {
   return (
@@ -16,9 +16,6 @@ const TwoFactorAuthenticationProfileNavbar = (props) => {
           <div className="text-wrappers">Left Title</div>
         </div>
         <div className="text-wrapper">Security</div>
-
-        {/* <div className="push-button">
-        </div> */}
       </div>
     </div>
   );
@@ -57,7 +54,9 @@ export default function TwoFactorAuthenticationProfile(props) {
 
   return (
     <TwoFactorAuthenticationProfileWrapper>
-      <TwoFactorAuthenticationProfileNavbar handelLeftTitle={props.handelLeftTitle} />
+      <TwoFactorAuthenticationProfileNavbar
+        handelLeftTitle={props.handelLeftTitle}
+      />
 
       <div className="Authentication-header">
         <Switch
@@ -92,32 +91,34 @@ export default function TwoFactorAuthenticationProfile(props) {
               placeholder="Enter Code"
               className="max-w-xs"
             />
-            <CustomButton classNames="sign-in-competent-sign-in" text="Next" onClick={
-                () => { setIsTurnedOn(true) ; setIsSelected(false)}
-            } />
+            <CustomButton
+              classNames="sign-in-competent-sign-in"
+              text="Next"
+              onClick={() => {
+                setIsTurnedOn(true);
+                setIsSelected(false);
+              }}
+            />
           </div>
         </div>
       ) : null}
 
-
-      {
-        AuthenticationisTurnedOn ?
+      {AuthenticationisTurnedOn ? (
         <div className="Authentication-aleardy-setups">
-                <Image
-                    shadow="none"
-                    radius="none"
-                    width="100%"
-                    alt={"2FA"}
-                    className="w-full object-cover fa-width h-[180px]"
-                    src={Icon2FASuccess}
-                />
-                <div className="Authentication-aleardy-setup-text">
-                        Two Factor Authentication is <span className="already">already setup</span>
-                </div>
-        </div> : null
-      }
-
-
+          <Image
+            shadow="none"
+            radius="none"
+            width="100%"
+            alt={"2FA"}
+            className="w-full object-cover fa-width h-[180px]"
+            src={Icon2FASuccess}
+          />
+          <div className="Authentication-aleardy-setup-text">
+            Two Factor Authentication is{" "}
+            <span className="already">already setup</span>
+          </div>
+        </div>
+      ) : null}
     </TwoFactorAuthenticationProfileWrapper>
   );
 }
