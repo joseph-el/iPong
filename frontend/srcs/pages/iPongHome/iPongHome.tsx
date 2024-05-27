bc7ae279d6a13fb28e239d16ec96d703805dfbaf
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:55:22 +0100

    Style: Add basic CSS styles to iPongHome page

---
+++ frontend/srcs/pages/iPongHome/iPongHome.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './iPongHome.css'
+
+import Logout from '../../components/UI/Logout/Logout';
+import ImessagesNotifications from '../../components/UI/iMessagesNotifications/iMessagesNotifications';
+
+import FriendNotifications from '../../components/UI/FriendNotifications/FriendNotifications';
+
+
+import NotificationsBar from '../../components/UI/NotificationsBar/NotificationsBar';
+export default function Home() {
+    return (
+        <div className="Home">
+            {/* <NotificationsBar /> */}
+          
+
+    
+
+        </div>
+    );
+};
