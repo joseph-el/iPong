0102d669eb26a58c9bb6d9a0bcc51a2271da9287
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:53:25 +0100

    Fix(alert): Simplify component structure
    Simplifies the iPongAlert component structure by removing unused dependencies and styling.

--- frontend/srcs/components/UI/iPongAlert/iPongAlert.tsx
+++ frontend/srcs/components/UI/iPongAlert/iPongAlert.tsx
@@ -1,90 +1,72 @@
 import React from "react";
-import './iPongAlert.css';
+import "./iPongAlert.css";
 
 import {
-    Tabs,
-    Tab,
-    Avatar,
-    Dropdown,
-    DropdownTrigger,
-    DropdownMenu,
-    Image,
-    DropdownItem,
-    Button,
-    Modal,
-    ModalContent,
-    ModalHeader,
-    ModalBody,
-    ModalFooter,
-    useDisclosure,
-    User,
-    Tooltip,
-  } from "@nextui-org/react";
+  Tabs,
+  Tab,
+  Avatar,
+  Dropdown,
+  DropdownTrigger,
+  DropdownMenu,
+  Image,
+  DropdownItem,
+  Button,
+  Modal,
+  ModalContent,
+  ModalHeader,
+  ModalBody,
+  ModalFooter,
+  useDisclosure,
+  User,
+  Tooltip,
+} from "@nextui-org/react";
 
 export default function IPongAlert(props) {
+  return (
+    <Modal backdrop={"blur"} isOpen={props.isOpen} onClose={props.onClose}>
+      <ModalContent>
+        {(onClose) => (
+          <>
+            <ModalHeader className="flex flex-col gap-1">
+              {" "}
+              {props.UserAlertHeader}{" "}
+            </ModalHeader>
+            <ModalBody>{props.UserAlertMessage}</ModalBody>
+            <ModalFooter>
+              <Button color="danger" variant="light" onPress={onClose}>
+                Closed
+              </Button>
+              <Button color="primary" onPress={onClose}>
+                {props.UserOptions}
+              </Button>
+            </ModalFooter>
+          </>
+        )}
+      </ModalContent>
+    </Modal>
+  );
 
-    return (
-                <Modal backdrop={"blur"} isOpen={props.isOpen} onClose={props.onClose}>
-                <ModalContent>
-                {(onClose) => (
-                    <>
-                    <ModalHeader className="flex flex-col gap-1">
-                        {" "}
-                        {props.UserAlertHeader}{" "}
-                    </ModalHeader>
-                    <ModalBody>
+  return (
+    <div className="alert-ipong">
+      <div className="content">
+        <div className="title">iPong</div>
+        <div className="description">{props.Description}</div>
+      </div>
 
-                        {props.UserAlertMessage}
-                        {/* {props.UserOptions === "Unfriend" ? (
-                        <p>Are you sure you want to unfriend this user?</p>
-                        ) : props.UserOptions === "Block" ? (
-                        <p>Are you sure you want to block this user?</p>
-                        ) : null} */}
-                    </ModalBody>
-                    <ModalFooter>
-                        <Button color="danger" variant="light" onPress={onClose}>
-                        Closed
-                        </Button>
-                        <Button color="primary" onPress={onClose}>
-                            {props.UserOptions}
-                        </Button>
-                    </ModalFooter>
-                    </>
-                )}
-                </ModalContent>
-            </Modal>
-    );
-
-
-
-    return (
-
-
-
-
-
-
-        <div className="alert-ipong">
-
-            <div className="content">
-                <div className="title">iPong</div>
-                <div className="description">{props.Description}</div>
-            </div>
-            
-            <div className="actions">
-                <div className="alert-item">
-                    <div className="action" onClick={props.handelLeftButton}>{props.leftButton}</div>
-                </div>
-                <div className="separator" />
-
-                <div className="action-wrapper">
-                    <div className="text-wrapper"  onClick={props.handelRightButton}>{props.rightButton}</div>
-                </div>
-            </div>
-
+      <div className="actions">
+        <div className="alert-item">
+          <div className="action" onClick={props.handelLeftButton}>
+            {props.leftButton}
+          </div>
         </div>
-    );
-  
-    
+        <div className="separator" />
 
+        <div className="action-wrapper">
+          <div className="text-wrapper" onClick={props.handelRightButton}>
+            {props.rightButton}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
