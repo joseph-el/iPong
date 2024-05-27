9914e9e6690beea4c7038882d580506a1aa1a4f5
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:50:09 +0100

    Fix: Adjust styles for delete button in friend notification
    Fixes the styling of the delete button in the friend notification component to align with the design mockups.

--- frontend/srcs/components/UI/FriendNotifications/FriendNotifications.tsx
+++ frontend/srcs/components/UI/FriendNotifications/FriendNotifications.tsx
@@ -1,47 +1,47 @@
 import React from "react";
-import './FriendNotifications.css';
-import {FriendNotificationsWrapper} from "./FriendNotificationsWrapper";
-import {User} from "@nextui-org/react";
+import "./FriendNotifications.css";
+import { FriendNotificationsWrapper } from "./FriendNotificationsWrapper";
+import { User } from "@nextui-org/react";
 
 export const Actions = (props) => {
-    return (
-        <div className="actions">
-
-            <div className="alert-item">
-                <div className="action" onClick={props.deleteButton}>Delete</div>
-            </div>
-
-            <div className="action-wrapper">
-                <div className="text-wrapper" onClick={props.confirmButton}>Confirm</div>
-            </div>
-
+  return (
+    <div className="actions">
+      <div className="alert-item">
+        <div className="action" onClick={props.deleteButton}>
+          Delete
         </div>
-    );
-}
+      </div>
 
+      <div className="action-wrapper">
+        <div className="text-wrapper" onClick={props.confirmButton}>
+          Confirm
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default function FriendNotifications(props) {
-
-    return (
-
-        <div className="FriendNotifications-frame">
-            <FriendNotificationsWrapper>
-                <div className="wrapper-style-request">
-                    <div className="FriendNotifications-content">
-                        <User   
-                            name="Friend Request"
-                            description={ props.name + " send you a friend request"}
-                            avatarProps={{
-                                src: props.avatar
-                            }}
-                        />
-                        <div className="FriendNotifications-time">
-                            {props.time}
-                        </div>
-                    </div>
-                </div>
-                <Actions confirmButton={props.confirmButton} deleteButton={props.deleteButton} />
-            </FriendNotificationsWrapper>
+  return (
+    <div className="FriendNotifications-frame">
+      <FriendNotificationsWrapper>
+        <div className="wrapper-style-request">
+          <div className="FriendNotifications-content">
+            <User
+              name="Friend Request"
+              description={props.name + " send you a friend request"}
+              avatarProps={{
+                src: props.avatar,
+              }}
+            />
+            <div className="FriendNotifications-time">{props.time}</div>
+          </div>
         </div>
-    );
+        <Actions
+          confirmButton={props.confirmButton}
+          deleteButton={props.deleteButton}
+        />
+      </FriendNotificationsWrapper>
+    </div>
+  );
 }
