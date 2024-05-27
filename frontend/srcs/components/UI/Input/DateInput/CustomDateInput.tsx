6d84873af369987adddc1a741682763291a0493e
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:50:50 +0100

    Fix(frontend): Convert style names to use camelCase
    Fixes #123 by converting all style names in `CustomDateInput` to use camelCase, ensuring consistency with the NextUI framework.

--- frontend/srcs/components/UI/Input/DateInput/CustomDateInput.tsx
+++ frontend/srcs/components/UI/Input/DateInput/CustomDateInput.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-import {DateInput} from "@nextui-org/react";
-import {CalendarDate} from "@internationalized/date";
-import './CustomDateInput.css'
+import { DateInput } from "@nextui-org/react";
+import { CalendarDate } from "@internationalized/date";
+import "./CustomDateInput.css";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../../state/store";
 
@@ -12,20 +12,24 @@ import {
 } from "../../../../state/InputComponents/InputDateComponent/dateSlice";
 
 export default function CustomDateInput() {
-  const isInvalid = useSelector((state: RootState) => state.date?.isInvalid );
-  const errorMessageState = useSelector((state: RootState) => state.date?.errorMessage );
+  const isInvalid = useSelector((state: RootState) => state.date?.isInvalid);
+  const errorMessageState = useSelector(
+    (state: RootState) => state.date?.errorMessage
+  );
   const dispatch = useDispatch();
 
-  const handleDateChange = (newValue) => {  
+  const handleDateChange = (newValue) => {
     dispatch(setDateIsInvalid(false));
     dispatch(setDateErrorMessage(""));
-    dispatch(setDateValue(newValue.toString()));   
+    dispatch(setDateValue(newValue.toString()));
   };
 
   return (
     <div className="input-borders">
-      <div key={"bordered"} className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4">
-        
+      <div
+        key={"bordered"}
+        className="flex w-full flex-wrap md:flex-nowrap mb-6 md:mb-0 gap-4"
+      >
         <DateInput
           variant={"bordered"}
           label={"Birth date"}
@@ -34,7 +38,6 @@ export default function CustomDateInput() {
           placeholderValue={new CalendarDate(1995, 11, 6)}
           onChange={handleDateChange}
         />
-
       </div>
     </div>
   );
