c997c1733823278629975c1b02c77ef1c219f61b
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:53:10 +0100

    fix: Fix stylesheet import issue in iMessagesNotifications
    Fixes an issue where the stylesheet was not imported correctly in the iMessagesNotifications component.

--- frontend/srcs/components/UI/iMessagesNotifications/iMessagesNotifications.tsx
+++ frontend/srcs/components/UI/iMessagesNotifications/iMessagesNotifications.tsx
@@ -1,27 +1,23 @@
 import React from "react";
-import './iMessagesNotifications.css';
+import "./iMessagesNotifications.css";
 import { ImessagesNotificationsWrapper } from "./iMessagesNotificationsWrapper";
-import ImessageIcon from './imessage.svg'
-import {User} from "@nextui-org/react";
+import ImessageIcon from "./imessage.svg";
+import { User } from "@nextui-org/react";
 
 export default function ImessagesNotifications(props) {
-    return (
-        <ImessagesNotificationsWrapper>
-            <div className="notification-content">
-                <User   
-                    name="iMessages"
-                    description={props.name +  " send you a message"}
-                    avatarProps={{
-                        src: props.avatar
-                    }}
-                />
-                <div className="time-of-notification">
-                    {props.time}
-                </div>
-                {
-                    /* <img src={ImessageIcon} alt="iMessage" className="imessage-icon"/> */ 
-                }
-            </div>
-        </ImessagesNotificationsWrapper>
-    );
+  return (
+    <ImessagesNotificationsWrapper>
+      <div className="notification-content">
+        <User
+          name="iMessages"
+          description={props.name + " send you a message"}
+          avatarProps={{
+            src: props.avatar,
+          }}
+        />
+        <div className="time-of-notification">{props.time}</div>
+        {/* <img src={ImessageIcon} alt="iMessage" className="imessage-icon"/> */}
+      </div>
+    </ImessagesNotificationsWrapper>
+  );
 }
