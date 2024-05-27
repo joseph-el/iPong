4372f57cb9468806a0921707377bf862e59f9177
Youssef El idrissi <josephmaryem@Youssefs-MacBook-Pro.local>
2024-05-27 18:48:32 +0100

    Refactor: Update LoginState enum values
    Changes the values of the LoginState enum to leverage bitwise operations.

--- frontend/srcs/components/AuthComponents/LoginState/LoginState.ts
+++ frontend/srcs/components/AuthComponents/LoginState/LoginState.ts
@@ -1,17 +1,17 @@
 export enum LoginState {
-    UNK = 0,
-    INVALID_PASSWORD = (1 << 0),
-    INVALID_EMAIL = (1 << 1),
-    LOGIN_SUCCESS = (1 << 2),
+  UNK = 0,
+  INVALID_PASSWORD = 1 << 0,
+  INVALID_EMAIL = 1 << 1,
+  LOGIN_SUCCESS = 1 << 2,
 
-    INVALID_NAME = (1 << 3),
-    INVALID_NAME_FORMAT = (1 << 4),
+  INVALID_NAME = 1 << 3,
+  INVALID_NAME_FORMAT = 1 << 4,
 
-    INVALID_EMAIL_FORMAT = (1 << 5),
-    INVALID_EMAIL_EXISTS = (1 << 6),
-    VALID_EMAIL = (1 << 7),
-    INVALID_PASSWORD_FORMAT = (1 << 8),
+  INVALID_EMAIL_FORMAT = 1 << 5,
+  INVALID_EMAIL_EXISTS = 1 << 6,
+  VALID_EMAIL = 1 << 7,
+  INVALID_PASSWORD_FORMAT = 1 << 8,
 
-    INVALID_DATE = (1 << 9),
-    INVALID_DATE_RANGE = (1 << 10),
+  INVALID_DATE = 1 << 9,
+  INVALID_DATE_RANGE = 1 << 10,
 }
