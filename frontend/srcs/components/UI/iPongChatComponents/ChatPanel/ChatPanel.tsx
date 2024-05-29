de9e6485593dfb60eac69c4b65e3a7072fc86d5f
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-05-29 13:33:13 +0100

    Refactor: Create ChatPanel component with initial design
    Introduces a new ChatPanel component and initial design, including a grid layout and placeholder content.

---
+++ frontend/srcs/components/UI/iPongChatComponents/ChatPanel/ChatPanel.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+import "./ChatPanel.css";
+
+import { Grid, GridItem } from "@chakra-ui/react";
+
+export default function ChatPanel() {
+  return (
+    <div className="ChatPanel-frame">
+      <Grid
+        templateAreas={`"header header"
+                        "main main"
+                        "main main"`}
+        gridTemplateRows={"60px 1fr 30px"}
+        gridTemplateColumns={"150px 1fr"}
+        h="100%"
+        w={"100%"}
+        gap="1"
+        color="blackAlpha.700"
+        fontWeight="bold"
+      >
+        <GridItem pl="2" w={"full"} h={"full"} area={"header"}>
+          Header
+        </GridItem>
+
+        <GridItem pl="2" w={"full"} h={"full"}  bg="green.300" area={"main"}>
+          Main
+        </GridItem>
+
+      </Grid>
+    </div>
+  );
+}
