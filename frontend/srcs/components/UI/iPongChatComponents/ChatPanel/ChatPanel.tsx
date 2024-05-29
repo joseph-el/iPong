496e76946d4d9fddc5374dafa8c05c4709d5502c
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-05-29 14:48:28 +0100

    Refactor: Update ChatPanel layout
    - Adds header and footer components to ChatPanel
    - Adjusts grid layout and styles

--- frontend/srcs/components/UI/iPongChatComponents/ChatPanel/ChatPanel.tsx
+++ frontend/srcs/components/UI/iPongChatComponents/ChatPanel/ChatPanel.tsx
@@ -3,27 +3,31 @@ import "./ChatPanel.css";
 
 import { Grid, GridItem } from "@chakra-ui/react";
 
+import ChatPanelHeader from "../ChatPanelHeader/ChatPanelHeader";
 export default function ChatPanel() {
   return (
     <div className="ChatPanel-frame">
       <Grid
         templateAreas={`"header header"
                         "main main"
-                        "main main"`}
-        gridTemplateRows={"55px 1fr 30px"}
-        gridTemplateColumns={"150px 1fr"}
+                        "Footer Footer"`}
+        gridTemplateRows={"90px 1fr 70px"}
+        // gridTemplateColumns={"10px 1fr"}
         h="100%"
         w={"100%"}
-        gap="1"
         color="blackAlpha.700"
         fontWeight="bold"
       >
         <GridItem pl="2" w={"full"} h={"full"} area={"header"}>
-          Header
+            <ChatPanelHeader/>
         </GridItem>
 
-        <GridItem pl="2" w={"full"} h={"full"}  bg="green.300" area={"main"}>
+        <GridItem pl="2" w={"full"} h={"full"}  bg={"black"} area={"main"}>
           Main
+        </GridItem>
+
+        <GridItem pl="2" w={"full"} h={"full"}  area={"Footer"}>
+          footer
         </GridItem>
 
       </Grid>
