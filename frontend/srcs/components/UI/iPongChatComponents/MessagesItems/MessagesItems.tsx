59075c221ceb9da56d760f32fab850a4a3f7b3fc
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-05-29 12:50:20 +0100

    feat(chat): enhance messages item design and add functionality
    Improves the design of the message items in the chat to enhance readability and adds functionality to select conversation.

---
+++ frontend/srcs/components/UI/iPongChatComponents/MessagesItems/MessagesItems.tsx
@@ -0,0 +1,54 @@
+import React from "react";
+import "./MessagesItems.css";
+import { Avatar } from "@nextui-org/react";
+
+import SelectConversationIcon from "./select-converstation-icon.svg";
+
+const handleLastMessageSize = (message) => {
+  const comparisonLength = 71;
+
+  console.log("data: ", message.length);
+  if (message.length > comparisonLength) {
+    return `${message.slice(0, comparisonLength)}...`;
+  } else return message;
+};
+
+const MessageLenght = (message) => {
+  console.log("margin:", message.lenght);
+  return message.lenght;
+};
+
+export default function MessagesItems(props) {
+  return (
+    <div className="MessagesItems-frame">
+      <div className="User-avatar">
+        <Avatar
+          src={props.avatar}
+          className="w-14 h-14 text-large"
+        />
+      </div>
+
+      <div className="Messages-info">
+
+        <div className="Name-and-Message-time">
+
+          <div className="Name">{props.name}</div>
+          <div className="time-and-select-conversation">
+            <div className="Message-time">{props.messageTime}</div>
+            <img
+              src={SelectConversationIcon}
+              alt="select-converstation"
+              className="select-conversation-icon"
+            />
+          </div>
+
+        </div>
+
+        <div className="last-Message">
+          {handleLastMessageSize(props.lastMessage)}
+        </div>
+
+      </div>
+    </div>
+  );
+}
