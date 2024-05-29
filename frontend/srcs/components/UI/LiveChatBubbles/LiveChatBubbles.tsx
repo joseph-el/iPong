e019206c0eeb4c7a0bedd12fc9a9e8536f85e31d
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-05-29 21:50:16 +0100

    Fix: Update LiveChatBubble component to display user avatar
    This commit updates the LiveChatBubble component to display the user's avatar image instead of a placeholder image.

--- frontend/srcs/components/UI/LiveChatBubbles/LiveChatBubbles.tsx
+++ frontend/srcs/components/UI/LiveChatBubbles/LiveChatBubbles.tsx
@@ -3,19 +3,18 @@ import "./LiveChatBubbles.css";
 
 import { Avatar } from "@nextui-org/avatar";
 export default function LiveChatBubbles(props) {
+  return (
+    <div className="LiveChatBubbles-frame">
+      <img
+        src={props.avatarLink}
+        alt="Avatar"
+        className="user-avatar"
+      />
 
-    return (
-        <div className="LiveChatBubbles-frame">
-            <Avatar src="https://avatars.dicebear.com/api/human/1.svg" alt="Avatar" size="sm" />
-            <div className="LiveChatBubbles-bubble">
-                <div className="fullname">
-                {props.fullname}
-                </div>
-                <div className="user-message">
-                {props.message}
-
-                </div>
-            </div>
-        </div>
-    );
+      <div className="LiveChatBubbles-bubble">
+        <div className="fullname">{props.fullname}</div>
+        <div className="user-message">{props.message}</div>
+      </div>
+    </div>
+  );
 }
