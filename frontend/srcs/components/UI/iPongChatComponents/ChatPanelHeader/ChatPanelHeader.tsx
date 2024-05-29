64652313f418d7ce7f2709e4e52ce70a846c0fa1
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-05-29 14:48:36 +0100

    Refactor: Update chat panel header with avatar and conversation details

--- frontend/srcs/components/UI/iPongChatComponents/ChatPanelHeader/ChatPanelHeader.tsx
+++ frontend/srcs/components/UI/iPongChatComponents/ChatPanelHeader/ChatPanelHeader.tsx
@@ -1,7 +1,19 @@
 import React from "react";
 import "./ChatPanelHeader.css";
 
+import {Avatar, AvatarGroup, AvatarIcon} from "@nextui-org/react";
+import ConversationDetailsIcon from "../MessagesItems/select-converstation-icon.svg";
 
-export default function ChatPanelHeader() {
-    
+export default function ChatPanelHeader(props) {
+    return (
+        <div className="ChatPanelHeader-frame">
+            <div className="ChatPanelHeader-title">
+                <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" className="w-14 h-14"/>
+                <div className="conversation-name-and-conversation-details">
+                    <div className="conversation-name">Taha Naceur</div>
+                    <img  src={ConversationDetailsIcon} alt="conversation-details" className="conversation-details"/>
+                </div>
+            </div>
+        </div>
+    )
 }
