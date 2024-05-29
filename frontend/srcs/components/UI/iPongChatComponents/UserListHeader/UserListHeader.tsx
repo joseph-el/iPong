21e156232b0b115ee8bfb6b10024e5d10f2ef28e
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-05-29 12:50:25 +0100

    Refactor: Add header to iPong chat component
    Adds a header to the iPong chat component, including a write new message button, title, and filtering buttons.

---
+++ frontend/srcs/components/UI/iPongChatComponents/UserListHeader/UserListHeader.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './UserListHeader.css';
+import WriteNewMessageIcon from './write_button.svg';
+import SearchInput from '../../Input/SearchInput/SearchInput';
+import {Chip} from "@nextui-org/react";
+
+export default function UserListHeader(props) {
+    return (
+        <div className='UserListHeader-frame'>
+            
+            {/* <SearchInput onChange={props.onChange} /> */}
+            <div className='write-button'>
+                <img src={WriteNewMessageIcon} alt='Write New Message' className='write-button-img' />
+            </div>
+
+            <div className='ipongchat-titile'>
+                Messages
+            </div>
+
+            <div className='filter-List-buttons'>
+                <Chip className='filter-List-buttons-chip'>All</Chip>
+                <Chip className='filter-List-buttons-chip'>Users</Chip>
+                <Chip className='filter-List-buttons-chip'>Groups</Chip>
+            </div>
+
+            
+
+        </div>  
+    );
+
+}
