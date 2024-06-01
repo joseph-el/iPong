da071298957726b6ce74cd66d2f4719b35e877f1
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-01 18:39:51 +0100

    adding svg paddles

---
+++ frontend/srcs/pages/iPongStore/assets/paddles/paddles.ts
@@ -0,0 +1,19 @@
+
+export { default as Paddles1 } from './paddles-1.svg';
+export { default as PaddlesEpicFlux } from './paddles-Epic Flux 1.svg';
+export { default as PaddlesLegendaryFlux } from './paddles-Legendary Flux.svg';
+export { default as PaddlesRareFlux } from './paddles-Rare Flux 1.svg';
+export { default as PaddlesCold } from './paddles-cold 1.svg';
+export { default as PaddlesFir3awn } from './paddles-fir3awn.svg';
+export { default as PaddlesHacker } from './paddles-hacker.svg';
+export { default as PaddlesJocker } from './paddles-jocker.svg';
+export { default as PaddlesLacasa } from './paddles-lacasa.svg';
+export { default as PaddlesNinja } from './paddles-ninja.svg';
+export { default as PaddlesPalyer10 } from './paddles-palyer10.svg';
+export { default as PaddlesPlayer20 } from './paddles-player20.svg';
+export { default as PaddlesSahrawi } from './paddles-sahrawi.svg';
+export { default as PaddlesSaudi } from './paddles-saudi.svg';
+export { default as PaddlesSpaceMan } from './paddles-space man.svg';
+export { default as PaddlesSpiderman } from './paddles-spiderman.svg';
+export { default as PaddlesTheDark } from './paddles-the_dark.svg';
+export { default as PaddlesZombie } from './paddles-zombie.svg';
