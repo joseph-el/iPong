fc0c2cefe9dfb1357bea8d8c4aeb023d25a6e5a8
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-01 16:00:03 +0100

    Fix: Add ShowCreateNewChat handler to UserListHeader
    This commit adds a ShowCreateNewChat handler to the UserListHeader component, allowing users to create new chats.

--- frontend/srcs/components/UI/iPongChatComponents/UserListHeader/UserListHeader.tsx
+++ frontend/srcs/components/UI/iPongChatComponents/UserListHeader/UserListHeader.tsx
@@ -1,31 +1,28 @@
-import React from 'react';
-import './UserListHeader.css';
-import WriteNewMessageIcon from './write_button.svg';
-import SearchInput from '../../Input/SearchInput/SearchInput';
-import {Chip} from "@nextui-org/react";
+import React from "react";
+import "./UserListHeader.css";
+import WriteNewMessageIcon from "./write_button.svg";
+import SearchInput from "../../Input/SearchInput/SearchInput";
+import { Chip } from "@nextui-org/react";
 
 export default function UserListHeader(props) {
-    return (
-        <div className='UserListHeader-frame'>
-            
-            {/* <SearchInput onChange={props.onChange} /> */}
-            <div className='write-button'>
-                <img src={WriteNewMessageIcon} alt='Write New Message' className='write-button-img' />
-            </div>
+  return (
+    <div className="UserListHeader-frame">
+      <div className="write-button">
+        <img
+          onClick={props.ShowCreateNewChat}
+          src={WriteNewMessageIcon}
+          alt="Write New Message"
+          className="write-button-img"
+        />
+      </div>
 
-            <div className='ipongchat-titile'>
-                Messages
-            </div>
+      <div className="ipongchat-titile">Messages</div>
 
-            <div className='filter-List-buttons'>
-                <Chip className='filter-List-buttons-chip'>All</Chip>
-                <Chip className='filter-List-buttons-chip'>Users</Chip>
-                <Chip className='filter-List-buttons-chip'>Groups</Chip>
-            </div>
-
-            
-
-        </div>  
-    );
-
+      <div className="filter-List-buttons">
+        <Chip className="filter-List-buttons-chip">All</Chip>
+        <Chip className="filter-List-buttons-chip">Users</Chip>
+        <Chip className="filter-List-buttons-chip">Groups</Chip>
+      </div>
+    </div>
+  );
 }
