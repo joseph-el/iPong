8f6f4df2881c9153431240acfdc6bbd4e9fd3c89
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-01 15:59:55 +0100

    Fix: Allow clicking on chat header to open chat settings
    Changes the ChatPanelHeader component to allow clicking on the header to open the chat settings.

--- frontend/srcs/components/UI/iPongChatComponents/ChatPanelHeader/ChatPanelHeader.tsx
+++ frontend/srcs/components/UI/iPongChatComponents/ChatPanelHeader/ChatPanelHeader.tsx
@@ -7,7 +7,7 @@ import ConversationDetailsIcon from "../MessagesItems/select-converstation-icon.
 export default function ChatPanelHeader(props) {
     return (
         <div className="ChatPanelHeader-frame">
-            <div className="ChatPanelHeader-title">
+            <div className="ChatPanelHeader-title" onClick={()  => {props.SetViewChatSettings} }>
                 <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" className="w-14 h-14"/>
                 <div className="conversation-name-and-conversation-details">
                     <div className="conversation-name">Taha Naceur</div>
