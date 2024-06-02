4012abebddcb06c45d2f76aa14cc4f827351dbb1
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-02 09:05:03 +0100

    Fix: Update item price text to "V-Bucks"
    Updates the "V-Buckets" text to "V-Bucks" throughout the list of items in the iPong store.

--- frontend/srcs/pages/iPongStore/iPongStore.tsx
+++ frontend/srcs/pages/iPongStore/iPongStore.tsx
@@ -26,72 +26,86 @@ const list = [
   {
     title: "Tilted Towers",
     img: TiltedTowers,
-    price: "500 V-Buckets",
+    price: "500 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Pleasant Park",
     img: PleasantPark,
-    price: "300 V-Buckets",
+    price: "300 V-Bucks",
+    isLocked: true,
   },
   {
     title: "Retail Row",
     img: RetailRow,
-    price: "1000 V-Buckets",
+    price: "1000 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Salty Springs",
     img: SaltySprings,
-    price: "530 V-Buckets",
+    price: "530 V-Bucks",
+    isLocked: true,
   },
   {
     title: "Loot Lake",
     img: LootLake,
-    price: "1570 V-Buckets",
+    price: "1570 V-Bucks",
+    isLocked: true,
   },
   {
     title: "Dusty Divot",
     img: DustyDivot,
-    price: "800 V-Buckets",
+    price: "800 V-Bucks",
+    isLocked: true,
   },
   {
     title: "Lazy Links",
     img: LazyLinks,
-    price: "750 V-Buckets",
+    price: "750 V-Bucks",
+    isLocked: true,
   },
   {
     title: "Tomato Temple",
     img: TomatoTemple,
-    price: "1220 V-Buckets",
+    price: "1220 V-Bucks",
+    isLocked: true,
   },
   {
     title: "Paradise Palms",
     img: ParadisePalms,
-    price: "900 V-Buckets",
+    price: "900 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Haunted Hills",
     img: HauntedHills,
-    price: "1100 V-Buckets",
+    price: "1100 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Shifty Shafts",
     img: ShiftyShafts,
-    price: "600 V-Buckets",
+    price: "600 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Greasy Grove",
     img: GreasyGrove,
-    price: "1450 V-Buckets",
+    price: "1450 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Flush Factory",
     img: FlushFactory,
-    price: "1000 V-Buckets",
+    price: "1000 V-Bucks",
+    isLocked: true,
   },
   {
     title: "Snobby Shores",
     img: SnobbyShores,
-    price: "850 V-Buckets",
+    price: "850 V-Bucks",
+    isLocked: true,
   },
 ];
 
@@ -99,89 +113,107 @@ const paddleList = [
   {
     title: "Epic Flux",
     img: PaddlesSVGs.PaddlesEpicFlux,
-    price: "500 V-Buckets",
+    price: "500 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Legendary Flux",
     img: PaddlesSVGs.PaddlesLegendaryFlux,
-    price: "300 V-Buckets",
+    price: "300 V-Bucks",
+    isLocked: true,
   },
   {
     title: "Rare Flux",
     img: PaddlesSVGs.PaddlesRareFlux,
-    price: "1000 V-Buckets",
+    price: "1000 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Cold Paddle",
     img: PaddlesSVGs.PaddlesCold,
-    price: "530 V-Buckets",
+    price: "530 V-Bucks",
+    isLocked: true,
   },
   {
     title: "Fire Awn",
     img: PaddlesSVGs.PaddlesFir3awn,
-    price: "1570 V-Buckets",
+    price: "1570 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Hacker Paddle",
     img: PaddlesSVGs.PaddlesHacker,
-    price: "800 V-Buckets",
+    price: "800 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Joker Paddle",
     img: PaddlesSVGs.PaddlesJocker,
-    price: "750 V-Buckets",
+    price: "750 V-Bucks",
+    isLocked: false,
   },
   {
     title: "La Casa Paddle",
     img: PaddlesSVGs.PaddlesLacasa,
-    price: "1220 V-Buckets",
+    price: "1220 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Ninja Paddle",
     img: PaddlesSVGs.PaddlesNinja,
-    price: "900 V-Buckets",
+    price: "900 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Paddles",
     img: PaddlesSVGs.PaddlesPalyer10,
-    price: "1100 V-Buckets",
+    price: "1100 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Player 20 Paddle",
     img: PaddlesSVGs.PaddlesPlayer20,
-    price: "600 V-Buckets",
+    price: "600 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Sahrawi Paddle",
     img: PaddlesSVGs.PaddlesSahrawi,
-    price: "1450 V-Buckets",
+    price: "1450 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Saudi Paddle",
     img: PaddlesSVGs.PaddlesSaudi,
-    price: "1000 V-Buckets",
+    price: "1000 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Space Man Paddle",
     img: PaddlesSVGs.PaddlesSpaceMan,
-    price: "850 V-Buckets",
+    price: "850 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Spiderman Paddle",
     img: PaddlesSVGs.PaddlesSpiderman,
-    price: "850 V-Buckets",
+    price: "850 V-Bucks",
+    isLocked: false,
   },
   {
     title: "The Dark Paddle",
     img: PaddlesSVGs.PaddlesTheDark,
-    price: "850 V-Buckets",
+    price: "850 V-Bucks",
+    isLocked: false,
   },
   {
     title: "Zombie Paddle",
     img: PaddlesSVGs.PaddlesZombie,
-    price: "850 V-Buckets",
+    price: "850 V-Bucks",
+    isLocked: false,
   },
 ];
+
 
 export default function IPongStore() {
   const [ShowPaddlesTabel, setShowPaddlesTabel] = React.useState(false);
@@ -200,7 +232,7 @@ export default function IPongStore() {
 
         <ScrollShadow hideScrollBar className="h-[450px] w-full">
           {!ShowPaddlesTabel ? (
-        <div className="gap-2 grid grid-cols-2 sm:grid-cols-3">
+        <div className="gap-2 grid grid-cols-2     sm:grid-cols-3">
         {list.map((item, index) => (
           <Card
             shadow="sm"
@@ -216,7 +248,8 @@ export default function IPongStore() {
                 width="100%"
                 isBlurred
                 alt={item.title}
-                className="w-full object-cover h-[140px]"
+               
+                className={"w-full object-cover h-[140px] " + (item.isLocked ? "blur-img" : "")}
                 src={item.img}
               />
             </CardBody>
@@ -239,19 +272,21 @@ export default function IPongStore() {
                 >
                   <CardBody className="overflow-visible p-0">
                     <Image
+                      
                       shadow="none"
                       radius="none"
                       width="100%"
                       isBlurred
               
                       alt={item.title}
-                      className="w-full object-coverd h-[120px]"
+                      className={"w-full object-coverd h-[120px] "  + (item.isLocked ? "blur-img" : "")}
                       src={item.img}
                     />
                   </CardBody>
                   <CardFooter className="text-small justify-between">
                     <b>{item.title}</b>
                     <p className="text-default-500">{item.price}</p>
+
                   </CardFooter>
                 </Card>
               ))}
