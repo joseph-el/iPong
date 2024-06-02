7786e60c730523818704dbce8f58aef2d7cbf782
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-02 15:29:35 +0100

    Refactor: Update User badge display for iPongGame
    Adds profile pictures from Pravatar for users in the game navigation bar.

--- frontend/srcs/pages/iPongGame/iPongGame.tsx
+++ frontend/srcs/pages/iPongGame/iPongGame.tsx
@@ -1,57 +1,61 @@
-import React from 'react';
+import React from "react";
 import "./iPongGame.css";
+import { User } from "@nextui-org/user";
 
-
-import Versus3d    from "./assets/Versus-3d.svg";
-import UserBadge   from "./assets/user-badge.svg";
+import Versus3d from "./assets/Versus-3d.svg";
+import UserBadge from "./assets/user-badge.svg";
 import VersusBadge from "./assets/versus-badge.svg";
 
 function UserBadgeComponent(props) {
-    return (
-        <div className="iPongGame-UserBadge">
-            <div className="iPongGame-UserBadge-username">
-                {props.username}
-            </div>
-            <img src={UserBadge} alt="UserBadge" />
-        </div>
-    )
+  return (
+    <div className="iPongGame-UserBadge">
+      <img src={UserBadge} alt="UserBadge" className="UserBadge" />
+      <div className="iPongGame-UserBadge-username">
+        <img
+          src="https://i.pravatar.cc/150?u=a04258114e29026702d"
+          alt="UserBadge"
+          className="UserBadge-img"
+        />
+        <div className="username">{props.username}</div>
+      </div>
+    </div>
+  );
 }
 
 function VersusBadgeComponent(props) {
-    return (
-        <div className="iPongGame-VersusBadge">
-            <img src={VersusBadge} alt="VersusBadge" />
-            <div className="iPongGame-UserBadge-username">
-                {props.username}
-            </div>
-        </div>
-    )
+  return (
+    <div className="iPongGame-VersusBadge">
+      <img src={VersusBadge} alt="VersusBadge" className="VersusBadge" />
+
+      <div className="iPongGame-UserBadge-username">
+        <img
+          src="https://i.pravatar.cc/150?u=a04258114e29026702d"
+          alt="UserBadge"
+          className="UserBadge-img UserBadge-img-vr"
+        />
+        <div className="username">{props.username}</div>
+      </div>
+    </div>
+  );
 }
 
- function IPongGameNav(props) {
-    return (
-        <div className="iPongGame-frame-nav">
-            <VersusBadgeComponent username={props.opponent} />
-            <img src={Versus3d} alt="Versus3d" className='Versus3d' />
-            <UserBadgeComponent username={props.username} />
-        </div>
-    )
+function IPongGameNav(props) {
+  return (
+    <div className="iPongGame-frame-nav">
+      <VersusBadgeComponent username={props.opponent} />
+      <img src={Versus3d} alt="Versus3d" className="Versus3d" />
+      <UserBadgeComponent username={props.username} />
+    </div>
+  );
 }
-
-
-
 
 export default function iPongGame() {
+  return (
+    <div className="iPongGame-frame">
+      <IPongGameNav username="Taha Naceur" opponent="youssef Naceur" />
 
-    return (
-        <div className="iPongGame-frame">
+        
 
-{/* <UserBadgeComponent username={"youusef"} /> */}
-
-            <IPongGameNav username="" opponent="" />
-        </div>
-    )
-
+    </div>
+  );
 }
-
-
