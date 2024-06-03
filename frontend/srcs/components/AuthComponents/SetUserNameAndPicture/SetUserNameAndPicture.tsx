4100613e7a3756b04f0b790bfd1833e0679c446a
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-03 12:45:33 +0100

    Fix: Update Avatar and add ScrollShadow to SetUserNameAndPicture component

--- frontend/srcs/components/AuthComponents/SetUserNameAndPicture/SetUserNameAndPicture.tsx
+++ frontend/srcs/components/AuthComponents/SetUserNameAndPicture/SetUserNameAndPicture.tsx
@@ -3,7 +3,7 @@ import "./SetUserNameAndPicture.css";
 import InputComponent from "../../UI/Input/Input";
 import Close from "../../UI/Button/CloseButton/CloseButton";
 import CustomButton from "../../UI/Button/SubmitButton/SubmitButton";
-import { Avatar } from "@nextui-org/react";
+import { Avatar, ScrollShadow } from "@nextui-org/react";
 import GestUser from "../assets/gest_user.svg";
 import { CameraIcon } from "../../UI/icon/CameraIcon";
 
@@ -80,59 +80,62 @@ export default function SetUserNameAndPicture() {
   };
 
   return (
-    <div className="SetUserNameAndPicture-frame max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
-      <div className="SetUserNameAndPicture">
-        <Close func={handelonClose} ClassName={"creat-close"} />
+    
+      <div className="SetUserNameAndPicture-frame max-w-md mx-auto rounded-xl shadow-md overflow-hidden h-[32rem] w-[25rem] sm:w-auto sm:h-auto 2xl:w-auto 2xl:h-auto ">
+        <div className="SetUserNameAndPicture">
+          <Close func={handelonClose} ClassName={"creat-close"} />
 
-        <div className="text-wrapper-3">Pick a profile picture</div>
+          <div className="text-wrapper-3">Pick a profile picture</div>
 
-        <div className="picture-target">
-          <Avatar
-            src={!selectedImage ? GestUser : selectedImage}
-            className="w-32 h-32 text-large"
-          />
+          <div className="picture-target">
+            <Avatar
+              isBordered
+              src={!selectedImage ? GestUser : selectedImage}
+              className="w-32 h-32 text-large"
+            />
 
-          <div className="camera-icon-padding">
-            <label htmlFor="fileInput">
-              <Avatar
-                className="camera-icon-size cursor-pointer"
-                showFallback
-                src="https://images.unsplash.com/broken"
-                fallback={
-                  <CameraIcon
-                    className="animate-pulse w-8 h-8 text-default-500"
-                    fill="currentColor"
-                    size={25}
-                  />
-                }
+            <div className="camera-icon-padding">
+              <label htmlFor="fileInput">
+                <Avatar
+                  className="camera-icon-size cursor-pointer"
+                  showFallback
+                  src="https://images.unsplash.com/broken"
+                  fallback={
+                    <CameraIcon
+                      className="animate-pulse w-[20px] h-[20px]"
+                      fill="currentColor"
+                      size={25}
+                    />
+                  }
+                />
+              </label>
+              <input
+                type="file"
+                id="fileInput"
+                accept="image/*"
+                style={{ display: "none" }}
+                onChange={handleImageChange}
               />
-            </label>
-            <input
-              type="file"
-              id="fileInput"
-              accept="image/*"
-              style={{ display: "none" }}
-              onChange={handleImageChange}
+            </div>
+          </div>
+
+          <div className="text-wrapper-4">What should we call you?</div>
+
+          <UserNameTitle />
+
+          <div className="input-padding">
+            <InputComponent
+              type={"fill"}
+              target={"username"}
+              id={"set-user-username"}
             />
           </div>
-        </div>
 
-        <div className="text-wrapper-4">What should we call you?</div>
-
-        <UserNameTitle />
-
-        <div className="input-padding">
-          <InputComponent
-            type={"fill"}
-            target={"username"}
-            id={"set-user-username"}
-          />
-        </div>
-
-        <div className="buttons-target" onClick={handelonSubmit}>
-          <CustomButton classNames="sign-in-competent-sign-in" text="Next" />
+          <div className="buttons-target" onClick={handelonSubmit}>
+            <CustomButton classNames="sign-in-competent-sign-in" text="Next" />
+          </div>
         </div>
       </div>
-    </div>
+
   );
 }
