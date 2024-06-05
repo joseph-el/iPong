ab5b1abe36c41a02cd7de2270b487196dd68d727
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-05 10:33:13 +0100

    Refactor: Removed duplicate console.log from getCurrentUser decorator

--- backend/src/auth/decorators/getCurrentUser.decorator.ts
+++ backend/src/auth/decorators/getCurrentUser.decorator.ts
@@ -5,7 +5,7 @@ export const GetCurrentUser = createParamDecorator(
   (data: keyof JwtPayloadWithRt | undefined, cntx: ExecutionContext) => {
     const request = cntx.switchToHttp().getRequest();
     const user = request.user;
-    console.log('userdsdsdsdsdsdsds:', user);
+    console.log(user);
     return data ? user && user[data] : user;
   },
 );
