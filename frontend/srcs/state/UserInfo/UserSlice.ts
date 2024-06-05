3895055e11bf96e0ec7d9fb93c1f400274060438
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-05 10:34:11 +0100

    Fix: Refactor UserSlice to match API schema

--- frontend/srcs/state/UserInfo/UserSlice.ts
+++ frontend/srcs/state/UserInfo/UserSlice.ts
@@ -1,39 +1,95 @@
-import { createSlice } from "@reduxjs/toolkit";
-
+import { createSlice } from '@reduxjs/toolkit';
 
 interface UserState {
-    id: number;
-    username: string;
-    password: string;
-    is_active: boolean;
+  id: string;
+  firstName: string;
+  lastName: string;
+  bio: string | null;
+  email: string;
+  githubLink: string | null;
+  isVerified: boolean;
+  level: number;
+  linkedInLink: string | null;
+  picture: string;
+  username: string;
+  FriendsCount: number;
 }
 
-const initialState : UserState = {
-    id: 0,
-    username: "",
-    password: "",
-    is_active: false,
+const initialState: UserState = {
+  id: '',
+  firstName: '',
+  lastName: '',
+  bio: null,
+  email: '',
+  githubLink: null,
+  isVerified: false,
+  level: 0,
+  linkedInLink: null,
+  picture: '',
+  username: '',
+  FriendsCount: 0,
 };
 
 const userState = createSlice({
-    name: "userState",
-    initialState,
-    reducers: { 
-        setId: (state, action) => {
-            state.id = action.payload;
-        },
-        setUsername: (state, action) => {
-            state.username = action.payload;
-        },
-        setPassword: (state, action) => {
-            state.password = action.payload;
-        },
-        setIsActive: (state, action) => {
-            state.is_active = action.payload;
-        },
+  name: 'userState',
+  initialState,
+  reducers: {
+    setId: (state, action) => {
+      state.id = action.payload;
     },
-  });
+    setFirstName: (state, action) => {
+      state.firstName = action.payload;
+    },
+    setLastName: (state, action) => {
+      state.lastName = action.payload;
+    },
+    setBio: (state, action) => {
+      state.bio = action.payload;
+    },
+    setEmail: (state, action) => {
+      state.email = action.payload;
+    },
+    setGithubLink: (state, action) => {
+      state.githubLink = action.payload;
+    },
+    setIsVerified: (state, action) => {
+      state.isVerified = action.payload;
+    },
+    setLevel: (state, action) => {
+      state.level = action.payload;
+    },
+    setLinkedInLink: (state, action) => {
+      state.linkedInLink = action.payload;
+    },
+    setPicture: (state, action) => {
+      state.picture = action.payload;
+    },
+    setUsername: (state, action) => {
+      state.username = action.payload;
+    },
+    setFriendsCount: (state, action) => {
+      state.FriendsCount = action.payload;
+    },
+    setUserProfile: (state, action) => {
+      return { ...state, ...action.payload };
+    },
+  },
+});
 
-export const { setId, setUsername, setPassword, setIsActive } = userState.actions;
+export const {
+  setId,
+  setFirstName,
+  setLastName,
+  setBio,
+  setEmail,
+  setGithubLink,
+  setIsVerified,
+  setLevel,
+  setLinkedInLink,
+  setPicture,
+  setUsername,
+  setFriendsCount,
+  setUserProfile,
+} = userState.actions;
+
 export default userState.reducer;
-
