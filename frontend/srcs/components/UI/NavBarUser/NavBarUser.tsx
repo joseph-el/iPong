dd94e51b72dd4fed57119c80460c0c4814e93e77
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-05 10:33:52 +0100

    Fix: Implemented dropdown menu in user navbar
    - Added dropdown menu with profile and logout options
    - Refactored user navbar component to use new dropdown menu
    - Updated styles and behavior of user navbar

--- frontend/srcs/components/UI/NavBarUser/NavBarUser.tsx
+++ frontend/srcs/components/UI/NavBarUser/NavBarUser.tsx
@@ -12,12 +12,16 @@ import {
   Button,
   User,
   Image,
-  Divider
+  Divider,
 } from "@nextui-org/react";
 
-import IpongLogo from "../../../components/SideBar/assets/iPongLogo.svg"
+import IpongLogo from "../../../components/SideBar/assets/iPongLogo.svg";
+
+import { useNavigate } from "react-router-dom";
+
 export default function NavBarUser(props) {
   const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+  const navigate = useNavigate();
 
   useEffect(() => {
     const handleResize = () => {
@@ -32,6 +36,14 @@ export default function NavBarUser(props) {
   const mailType = props.email.split("@")[1];
   const isLongEmail = userName.length > 5 && windowWidth < 1150;
   let truncatedUserName = isLongEmail ? userName.substring(0, 4) : userName;
+
+  const handleOnClick = (mode) => {
+    if (mode) {
+      navigate("/ipong/profile");
+    } else {
+      navigate("/");
+    }
+  };
   return (
     <div className="flex items-center gap-4 NavBarUser w-max-content  ">
       <Dropdown
@@ -43,9 +55,7 @@ export default function NavBarUser(props) {
           content: "p-0 border-small border-divider bg-background",
         }}
       >
-        <DropdownTrigger onClick={props.onClick} 
-          
-        >
+        <DropdownTrigger onClick={props.onClick}>
           <User
             name={props.fullName + " "}
             description={
@@ -58,231 +68,26 @@ export default function NavBarUser(props) {
         </DropdownTrigger>
 
         <DropdownMenu aria-label="Static Actions">
-
-        {/* <DropdownItem key="SeeProfile"
-          isDisabled>
-            <Image src={IpongLogo} style={{ margin: "20px 20px 10px 55px" }} alt="avatar" width={70} height={70} />
-        </DropdownItem> */}
-
-        <DropdownItem isSelected variant="faded">
-          See Profile
-   
-          
+          <DropdownItem
+            onClick={() => {
+              handleOnClick(true);
+            }}
+            variant="faded"
+          >
+            See Profile
           </DropdownItem>
-        <DropdownItem  isSelected  variant="faded" className="text-danger" color="danger">
-          Log Out
-        </DropdownItem>
-      </DropdownMenu>
+          <DropdownItem
+            onClick={() => {
+              handleOnClick(false);
+            }}
+            variant="faded"
+            className="text-danger"
+            color="danger"
+          >
+            Log Out
+          </DropdownItem>
+        </DropdownMenu>
       </Dropdown>
-
-      {/*
-                <Dropdown placement="bottom-end" closeOnSelect={true}>
-                        <DropdownTrigger onClick={props.onClick} >
-                            <User    
-                                name={props.fullName + " "}
-                                description={truncatedUserName + (isLongEmail ? '..' : '') + '@' + mailType}
-                                avatarProps={{
-                                    src: props.avatar,
-                                }}     
-                                />
-                        </DropdownTrigger>
-                        
-                        <DropdownMenu
-                            aria-label="Custom item styles"
-                            disabledKeys={["profile"]}
-                            className="p-3"
-      
-                        >
-
-                                
-
-                            <DropdownSection aria-label="Profile & Actions" showDivider>
-                                <DropdownItem
-                                    isReadOnly
-                                    key="profile"
-                                    className="h-14 gap-2"
-                                    className="opacity-100"
-                                >
-                                  <User
-                                    name={props.fullName}
-                                    description={"@" + props.username}
-                                    classNames={{
-                                        name: "text-default-600",
-                                        description: "text-default-500",
-                                    }}
-                                    avatarProps={{
-                                        size: "sm",
-                                        src: props.avatar,
-                                    }}
-                                  />
-
-                                </DropdownItem>
-
-                                <DropdownItem key="dashboard">
-                                    See Profile
-                                </DropdownItem>
-
-                            </DropdownSection>
-
-
-                            <DropdownSection aria-label="Help & Feedback">
-                              <DropdownItem key="help_and_feedback">
-                                  Help & Feedback
-                              </DropdownItem>
-
-                              <DropdownItem key="logout" className="text-danger" color="danger">
-                                Log Out
-                              </DropdownItem>
-                            </DropdownSection> 
-                        </DropdownMenu>
-                </Dropdown>
-              */}
     </div>
   );
 }
-
-/*
-
-
-            <Dropdown
-              showArrow
-              radius="sm"
-              classNames={{
-                base: "before:bg-default-200", // change arrow background
-                content: "p-0 border-small border-divider bg-background",
-              }}
-            >
-              <DropdownTrigger>
-                <Button variant="ghost" disableRipple>Open Menu</Button>
-              </DropdownTrigger>
-              <DropdownMenu
-                aria-label="Custom item styles"
-                disabledKeys={["profile"]}
-                className="p-3"
-                itemClasses={{
-                  base: [
-                    "rounded-md",
-                    "text-default-500",
-                    "transition-opacity",
-                    "data-[hover=true]:text-foreground",
-                    "data-[hover=true]:bg-default-100",
-                    "dark:data-[hover=true]:bg-default-50",
-                    "data-[selectable=true]:focus:bg-default-50",
-                    "data-[pressed=true]:opacity-70",
-                    "data-[focus-visible=true]:ring-default-500",
-                  ],
-                }}
-              >
-                <DropdownSection aria-label="Profile & Actions" showDivider>
-                  <DropdownItem
-                    isReadOnly
-                    key="profile"
-                    className="h-14 gap-2"
-                    className="opacity-100"
-                  >
-                    <User
-                      name="Junior Garcia"
-                      description="@jrgarciadev"
-                      classNames={{
-                        name: "text-default-600",
-                        description: "text-default-500",
-                      }}
-                      avatarProps={{
-                        size: "sm",
-                        src: "https://avatars.githubusercontent.com/u/30373425?v=4",
-                      }}
-                    />
-                  </DropdownItem>
-                  <DropdownItem key="dashboard">
-                    Dashboard
-                  </DropdownItem>
-                  <DropdownItem key="settings">Settings</DropdownItem>
-                  <DropdownItem
-                    key="new_project"
-                    endContent={<PlusIcon className="text-large" />}
-                  >
-                    New Project
-                  </DropdownItem>
-                </DropdownSection>
-        
-                <DropdownSection aria-label="Preferences" showDivider>
-                  <DropdownItem key="quick_search" shortcut="⌘K">
-                    Quick search
-                  </DropdownItem>
-                  <DropdownItem
-                    isReadOnly
-                    key="theme"
-                    className="cursor-default"
-                    endContent={
-                      <select
-                        className="z-10 outline-none w-16 py-0.5 rounded-md text-tiny group-data-[hover=true]:border-default-500 border-small border-default-300 dark:border-default-200 bg-transparent text-default-500"
-                        id="theme"
-                        name="theme"
-                      >
-                        <option>System</option>
-                        <option>Dark</option>
-                        <option>Light</option>
-                      </select>
-                    }
-                  >
-                    Theme
-                  </DropdownItem>
-                </DropdownSection>  
-        
-                <DropdownSection aria-label="Help & Feedback">
-                  <DropdownItem key="help_and_feedback">
-                    Help & Feedback
-                  </DropdownItem>
-                  <DropdownItem key="logout">Log Out</DropdownItem>
-                </DropdownSection> 
-              </DropdownMenu>
-            </Dropdown>
- 
-        
-
-
-// 
-  
-*/
-
-/*
-export default function NavBarUser(props) {
-    const [windowWidth, setWindowWidth] = useState(window.innerWidth);
-
-    useEffect(() => {
-        const handleResize = () => {
-            setWindowWidth(window.innerWidth);
-        };
-        window.addEventListener('resize', handleResize);
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, []);
-
-    const userName = props.email.split('@')[0];
-    const mailType = props.email.split('@')[1];
-
-    const isLongEmail = userName.length > 5 && windowWidth < 1150;
-
-    let truncatedUserName = isLongEmail ? userName.substring(0, 4) : userName;
-
-    return (
-        <>
-            <div className="NavBarUser w-max-content">
-                <User
-                    name={props.fullName}
-                    description={truncatedUserName + (isLongEmail ? '..' : '') + '@' + mailType}
-                    avatarProps={{
-                        src: props.avatar,
-                    }}
-                    
-                />
-                <div className='user-info'>
-                    <img src={DropDownArrow} alt='arrow down' onClick={props.onClick} className="img-info"/>
-                </div>
-            </div>
-        </>
-    );
-}
-
-*/
