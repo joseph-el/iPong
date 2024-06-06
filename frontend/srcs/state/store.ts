e54aa1ac95b2e6238ea0f3998aa5c55c65ebaeb3
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-06 23:39:05 +0100

    feat: Introduce Achievement Slice in store
    Adds a reducer for achievements to the application store.

--- frontend/srcs/state/store.ts
+++ frontend/srcs/state/store.ts
@@ -3,15 +3,17 @@ import inputReducer from "./InputComponents/inputSlice"
 import dateSlice from "./InputComponents/InputDateComponent/dateSlice"
 import genderSlice from './Checkbox/CheckboxSlice';
 import UserSlice from './UserInfo/UserSlice';
+import achievementSlice from './Achievement/AchievementSlice';
+
 export const store = configureStore({
     reducer: {
       input: inputReducer,
       date: dateSlice,
       gender: genderSlice,
       userState: UserSlice,
+      achievement: achievementSlice,
     },
 });
-
 
 export type AppDispatch = typeof store.dispatch
 export type RootState = ReturnType<typeof store.getState>
