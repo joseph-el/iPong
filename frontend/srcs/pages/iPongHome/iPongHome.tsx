24208de2c5018d6de816ae18ff0b851d288eddee
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-06 23:38:47 +0100

    Refactor: Implement iPong game page and handle achievement badges
    Changes the iPong game page to implement bot mode and online battling. Also includes handling for achievement badges.

--- frontend/srcs/pages/iPongHome/iPongHome.tsx
+++ frontend/srcs/pages/iPongHome/iPongHome.tsx
@@ -1,21 +1,83 @@
-import React from 'react'
-import './iPongHome.css'
+import React from "react";
+import "./iPongHome.css";
 
-import Logout from '../../components/UI/Logout/Logout';
-import ImessagesNotifications from '../../components/UI/iMessagesNotifications/iMessagesNotifications';
+import { useLocation, useNavigate } from "react-router-dom";
+import { PATHS } from "../../game/constants/paths";
+import BotMode from "../../game/components/BotMode/BotMode";
+import MatchMaking from "../../game/components/MatchMaking/MatchMaking";
 
-import FriendNotifications from '../../components/UI/FriendNotifications/FriendNotifications';
+import { useEffect, useState } from "react";
 
+import CongratulationsBadge from "../../components/UI/AchievementComponents/CongratulationsBadge/CongratulationsBadge";
+import { setAchievementBadge } from "../../state/Achievement/AchievementSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../state/store";
 
-import NotificationsBar from '../../components/UI/NotificationsBar/NotificationsBar';
-export default function Home() {
-    return (
-        <div className="Home">
-            {/* <NotificationsBar /> */}
-          
+export default function iPongGame() {
+  type ModeType = "practice" | "onlineBattle" | null;
+  const [mode, setMode] = useState<ModeType>(null);
+  const [urlMode, setUrlMode] = useState("default");
+  const location = useLocation();
+  const navigate = useNavigate();
 
-    
+  /* i checked the path to extract coming from: mode */
+  useEffect(() => {
+    const searchParams = new URLSearchParams(location.search);
+    const modeParam = searchParams.get("mode");
+    if (modeParam === "default") {
+      setUrlMode("default");
+      setMode(null);
+    }
+  }, [location.search, urlMode]);
 
+  function vsBotHandler() {
+    setMode("practice");
+    navigate(PATHS.PRACTICE_MODE);
+  }
+
+  function vsRandomHandler() {
+    setMode("onlineBattle");
+    navigate(PATHS.ONLINE_RANDOM_MODE);
+  }
+
+  /* Check What Mode To Render */
+  if (mode && mode === "practice") {
+    return <BotMode />;
+  }
+  if (mode && mode === "onlineBattle") {
+    return <MatchMaking />;
+  }
+
+  const dispatch = useDispatch<AppDispatch>();
+  const UserInfo = useSelector((state: RootState) => state.userState);
+
+  const Achievement = useSelector((state: RootState) => state.achievement);
+
+  const handleCloseCongratulationsBadge = () => {
+    dispatch(setAchievementBadge(null));
+  };
+
+  return (
+    <div className="container--home">
+      <div className="container-nav">
+        <button className="button" onClick={vsBotHandler}>
+          Play vs Bot
+        </button>
+        <button className="button" onClick={vsRandomHandler}>
+          Play vs Random
+        </button>
+      </div>
+
+      {/* {Achievement != null ? ( */}
+        <div className="blur-background">
+          <div className="AchievementList-place fade-in">
+            <CongratulationsBadge
+              level={Achievement}
+              CongratulationsBadge={handleCloseCongratulationsBadge}
+            />
+          </div>
         </div>
-    );
-};
+      {/* ) : null} */}
+    </div>
+  );
+}
