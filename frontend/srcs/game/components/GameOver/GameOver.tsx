c4974b50ef550ff8da84527bef0fef482215878b
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-07 11:49:28 +0100

    Fix: Add achievement badge on level up during game over

--- frontend/srcs/game/components/GameOver/GameOver.tsx
+++ frontend/srcs/game/components/GameOver/GameOver.tsx
@@ -3,6 +3,12 @@ import { useNavigate } from "react-router-dom";
 import { PATHS } from "../../constants/paths";
 import "./GameOver.css";
 
+import { setAchievementBadge } from "../../../state/Achievement/AchievementSlice";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../state/store";
+import { getUserLevel } from "../../../utils/getCurrentLevel";
+import api from "../../../api/posts";
+
 interface GameOverProps {
   winner: string | null;
   winnerId?: string | null;
@@ -12,10 +18,36 @@ export default function GameOver({ winner, winnerId }: GameOverProps) {
   const [visible, setVisible] = useState(true);
   const navigate = useNavigate();
 
+  const dispatch = useDispatch<AppDispatch>();
+  const UserInfo = useSelector((state: RootState) => state.userState);
+
+
   useEffect(() => {
     const timer = setTimeout(() => {
       setVisible(false);
+      
+      const LevelstoredValue = localStorage.getItem('lastLevel');
+      const lastLevel = LevelstoredValue ? parseInt(LevelstoredValue) : 0;
+
+
+
+      try {
+          const respose = await api.get(`/me
+      } catch (error) {
+
+
+      }
+      const currentLevel = getUserLevel(UserInfo.xp);
+
+      console.log("currentLevel", currentLevel);
+      console.log("lastLevel", lastLevel);
+
+      if (currentLevel > lastLevel) {
+        dispatch(setAchievementBadge(currentLevel));
+      }
+  
       navigate(PATHS.DEFAULT_GAME_PAGE);
+
     }, 4000);
 
     return () => clearTimeout(timer);
