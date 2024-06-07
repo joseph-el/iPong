e1b4e26a6cfa01af2afcdc91af9846e70d7cd7c6
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-07 14:37:44 +0100

    feat(GameOver): Calculate and set achieved badge based on new xp

--- frontend/srcs/game/components/GameOver/GameOver.tsx
+++ frontend/srcs/game/components/GameOver/GameOver.tsx
@@ -13,43 +13,33 @@ import { set } from "lodash";
 interface GameOverProps {
   winner: string | null;
   winnerId?: string | null;
+  winnerXp?: number;
+  loserXp?: number;
 }
 
-export default function GameOver({ winner, winnerId }: GameOverProps) {
+export default function GameOver({
+  winner,
+  winnerId,
+  winnerXp,
+  loserXp,
+}: GameOverProps) {
   const [visible, setVisible] = useState(true);
   const navigate = useNavigate();
 
   const dispatch = useDispatch<AppDispatch>();
-  const  UserInfo = useSelector((state: RootState) => state.userState);
-  const [current, setCurrent] = useState(0);
+  const UserInfo = useSelector((state: RootState) => state.userState);
+  const [UpdatedLevel, setUpdatedLevel] = useState(0);
 
+  
   useEffect(() => {
-
-
-    const fetchUser = async () => {
     const LevelstoredValue = localStorage.getItem("lastLevel");
-    console.log("LevelstoredValueonStore ", LevelstoredValue);
     const lastLevel = LevelstoredValue ? parseInt(LevelstoredValue) : 0;
+    setUpdatedLevel(winnerId === UserInfo.id ? winnerXp! : loserXp!);
 
-    try {
-      const response =  await api.get("/user-profile/me");
-      console.log("response", response);
-      
-      setCurrent(response.data.xp);
-    } catch (error) {
-      console.log("error", error);
+    if (getUserLevel(UpdatedLevel) > lastLevel) {
+      dispatch(setAchievementBadge(UpdatedLevel));
+      localStorage.setItem("lastLevel", getUserLevel(UpdatedLevel).toString());
     }
-    
-    const currentLevel = getUserLevel(current);
-
-    console.log("currentLevel", currentLevel);
-    console.log("lastLevel", lastLevel);
-
-    if (currentLevel > lastLevel) {
-      dispatch(setAchievementBadge(currentLevel));
-    }
-  }
-  fetchUser();
   }, []);
 
   useEffect(() => {
