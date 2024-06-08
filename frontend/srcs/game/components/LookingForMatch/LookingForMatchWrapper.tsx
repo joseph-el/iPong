840888155f56b61624b403afbd0cb0316ac3d346
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-08 22:08:04 +0100

    Coding looking for match components

---
+++ frontend/srcs/game/components/LookingForMatch/LookingForMatchWrapper.tsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./LookingForMatch.css";
+
+export const LookingForMatchWrapper = ({ children }) => (
+  <div className="LookingForMatchWrapper-box">{children}</div>
+);
