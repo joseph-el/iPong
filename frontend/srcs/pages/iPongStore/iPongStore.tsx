c993b994f2d2a7d4205bb006e3e65acfb1f3085e
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-10 12:15:05 +0100

    feat(frontend): Implement item purchase in iPongStore
    - Introduced item purchase functionality for skins and boards in the iPongStore.
    - Added item confirmation and insufficient funds alerts.
    - Implemented logic to handle item unlocks and selections.
    - Updated display to show unlock status and selected items.

--- frontend/srcs/pages/iPongStore/iPongStore.tsx
+++ frontend/srcs/pages/iPongStore/iPongStore.tsx
@@ -17,7 +17,9 @@ import { SKIN_DB } from "./db/skins.db";
 
 import { useEffect, useState } from "react";
 import api from "../../api/posts";
-
+import IPongAlert from "../../components/UI/iPongAlert/iPongAlert";
+import { useDisclosure } from "@nextui-org/react";
+import CongratulationsNewSkin from "../../components/UI/CongratulationsNewItem/CongratulationsNewSkin";
 /*
 const list = [
   {
@@ -212,12 +214,51 @@ const paddleList = [
 ];
 */
 
+let closed = false;
+
+const setClosed = (value) => {
+  closed = value;
+};
+
 export default function IPongStore() {
+  const UserInfo = useSelector((state: RootState) => state.userState);
   const dispatch = useDispatch<AppDispatch>();
   const [ShowPaddlesTabel, setShowPaddlesTabel] = useState(false);
 
   const [AvailableBoards, setAvailableBoards] = useState([]);
   const [AvailablePaddles, setAvailablePaddles] = useState([]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
+
+
+  const [ItemBuySuccess, setItemBuySuccess] = useState<[] | null>(null);
+
+  enum BUY_TYPES {
+    UNK,
+    BUY_BOARD,
+    BUY_PADDLE,
+  }
+  const [BuyItem, setBuyItem] = useState([]);
+
+  const AlertProps = (itemName) => {
+    return [
+      {},
+      {
+        UserAlertHeader: `Confirm Purchase: ${itemName}`,
+        UserAlertMessage: `Would you like to purchase ${itemName}? Once purchased, it will be added to your inventory and the amount will be deducted from your balance.`,
+        UserOptions: "Buy",
+        CloseButton: true,
+      },
+      {
+        UserAlertHeader: `Insufficient iCoins`,
+        UserAlertMessage: `You do not have enough iCoins to purchase ${itemName}`,
+        UserOptions: "OK",
+        CloseButton: false,
+      },
+    ];
+  };
+
+  const [showAlert, setShowAlert] = useState(AlertProps("")[0]);
 
   useEffect(() => {
     const fetchBoardsData = async () => {
@@ -272,20 +313,91 @@ export default function IPongStore() {
       }
     };
 
-    fetchBoardsData();
-    fetchPaddlesData();
-  }, []);
+    if (ItemBuySuccess === null) {
+      fetchBoardsData();
+      fetchPaddlesData();
+    }
+  }, [ItemBuySuccess]);
 
-  console.log("boards: ", AvailableBoards);
-  console.log("paddles: ", AvailablePaddles);
+  useEffect(() => {
+    const postBoardsData = async () => {
+      try {
+        const response = await api.post("/boards/add", {
+          BoardName: BuyItem.item.name,
+        });
+        if (response.status === 201) {
+          const BuyedSkin = BOARDS_DB.find(
+            (item) => item.name === BuyItem.item.name
+          );
+          setItemBuySuccess(BuyedSkin);
+          console.log("buy State: ", BuyedSkin);
+        }
+      } catch (error) {
+        console.log("Error buying board", error);
+      }
+    };
+    const postPaddlesData = async () => {
+      try {
+        const response = await api.post("/skins/add", {
+          SkinName: BuyItem.item.name,
+        });
+        if (response.status === 201) {
+          const BuyedSkin = SKIN_DB.find(
+            (item) => item.name === BuyItem.item.name
+          );
+          setItemBuySuccess(BuyedSkin);
+        }
+        console.log("buy State: ", response.data);
+      } catch (error) {
+        console.log("Error buying paddle", error);
+      }
+    };
+    if (BuyItem.ReadyToBuy == undefined || !BuyItem.ReadyToBuy) return;
+    BuyItem.type === BUY_TYPES.BUY_BOARD && postBoardsData();
+    BuyItem.type === BUY_TYPES.BUY_PADDLE && postPaddlesData();
+  }, [BuyItem]);
+
+  const handelBuyItem = () => {
+    if (closed) {
+      setClosed(false);
+      onClose();
+      return;
+    }
+    if (BuyItem.item.price > UserInfo.wallet) {
+      setShowAlert(AlertProps(BuyItem.item?.name)[2]);
+      setClosed(true);
+      console.log("Closed: innnn ", closed);
+      onClose();
+      onOpen();
+      return;
+    }
+    setBuyItem({ ...BuyItem, ReadyToBuy: true });
+  };
+
   const handelPressPaddlesTabel = (item) => {
     console.log(item);
-    dispatch(setBoardPath(item.img));
+    if (!item.isUnlocked) {
+      setShowAlert(AlertProps(item?.name)[1]);
+      onOpen();
+
+      setBuyItem({ type: BUY_TYPES.BUY_BOARD, item: item });
+      return;
+    }
+    console.log(item);
+    localStorage.setItem("userBoard", item.name);
+    dispatch(setBoardPath(item.name));
   };
 
   const handelPressPaddles = (item) => {
+    if (!item.isUnlocked) {
+      setShowAlert(AlertProps(item?.name)[1]);
+      onOpen();
+      setBuyItem({ type: BUY_TYPES.BUY_PADDLE, item: item });
+      return;
+    }
     console.log(item);
-    dispatch(setSelectedSkinPath(item.img));
+    localStorage.setItem("userSkin", item.name);
+    dispatch(setSelectedSkinPath(item.name));
   };
 
   return (
@@ -337,7 +449,14 @@ export default function IPongStore() {
                   </CardBody>
                   <CardFooter className="text-small justify-between">
                     <b>{item.name}</b>
-                    <p className="text-default-500">{(item.isUnlocked) ? "" :  (item.price + " iCoins")}</p>
+                    <p className="text-default-500">
+                      {item.isUnlocked ? "" : item.price + " iCoins"}
+                    </p>
+
+                    {item.isUnlocked &&
+                    UserInfo.userSelectedBoardPath === item.name ? (
+                      <b className="text-default-500">Selected</b>
+                    ) : null}
                   </CardFooter>
                 </Card>
               ))}
@@ -367,14 +486,43 @@ export default function IPongStore() {
                   </CardBody>
                   <CardFooter className="text-small justify-between">
                     <b>{item.name}</b>
-                    <p className="text-default-500">{(item.isUnlocked) ? "" : (item.price + " iCoins") }</p>
+                    <p className="text-default-500">
+                      {item.isUnlocked ? "" : item.price + " iCoins"}
+                    </p>
+                    {item.isUnlocked &&
+                    UserInfo.userSelectedSkinPath === item.name ? (
+                      <b className="text-default-500">Selected</b>
+                    ) : null}
                   </CardFooter>
                 </Card>
               ))}
             </div>
           )}
         </ScrollShadow>
+
+        <IPongAlert
+          hideCloseButton={showAlert.CloseButton}
+          handelRemoveUser={handelBuyItem}
+          isOpen={isOpen}
+          onClose={onClose}
+          UserAlertHeader={showAlert.UserAlertHeader}
+          UserAlertMessage={showAlert.UserAlertMessage}
+          UserOptions={showAlert.UserOptions}
+        ></IPongAlert>
       </div>
+
+      {ItemBuySuccess != null && (
+        <div className="blur-background">
+          <div className="AchievementList-place fade-in">
+            <CongratulationsNewSkin
+              close={() => setItemBuySuccess(null)}
+              imgPath={ItemBuySuccess.imgPath}
+              description={ItemBuySuccess.description}
+              name={ItemBuySuccess.name}
+            />
+          </div>
+        </div>
+      )}
     </div>
   );
 }
