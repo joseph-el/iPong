2e903f29527510be8605b6d0cde4b1e1c9672122
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-10 12:15:24 +0100

    feat: Redesign iPongChatState to manage messages and user lists
    Reintroduces old state management and refactors messages and user lists to be managed through the state instead of context.

--- frontend/srcs/state/iPongChatState/iPongChatState.ts
+++ frontend/srcs/state/iPongChatState/iPongChatState.ts
@@ -1,28 +1,66 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { set } from "lodash";
 
-interface ChatState {
-    isInvalid: boolean;
-    errorMessage: string;
-    value: [];
-  }
-  
-  const initialState : ChatState = {
-    isInvalid: false,
-    errorMessage: "",
-    value: [],
-  };
+interface Messages {
+  id: string;
+  message: string;
+  user: string;
+  time: string;
+}
+interface ListMessages {
+  id: string;
+  fullname: string;
+  lastMessage: string;
+  time: string;
+}
 
+interface iPongChatState {
+  messages: Messages[];
+  ListMessages: ListMessages[];
+  selectedUser: string;
+  UserSetting: Boolean;
+  GroupSetting: Boolean;
+}
 
-  const inputSlice = createSlice({
-    name: "iPongChat",
-    initialState: {},
-    reducers: {
-      setIsInvalid: (state, action) => {
-      },
+const initialState: iPongChatState = {
+  messages: [],
+  ListMessages: [],
+  selectedUser: "",
+  UserSetting: false,
+  GroupSetting: false,
+};
 
+const inputSlice = createSlice({
+  name: "iPongChat",
+  initialState,
+  reducers: {
+    setListMessages(state, action) {
+      state.ListMessages = action.payload;
     },
-  });
-  
-  
-  export const {  setIsInvalid } = inputSlice.actions;
-  export default inputSlice.reducer;
+    addMessage(state, action) {
+      state.messages.push(action.payload);
+    },
+    setMessages(state, action) {
+      state.messages = action.payload;
+    },
+    selectUser(state, action) {
+      state.selectedUser = action.payload;
+    },
+    setUserSetting(state, action) {
+      state.UserSetting = action.payload;
+    },
+    setGroupSetting(state, action) {
+      state.GroupSetting = action.payload;
+    },
+  },
+});
+
+export const {
+  addMessage,
+  setMessages,
+  selectUser,
+  setUserSetting,
+  setGroupSetting,
+  setListMessages,
+} = inputSlice.actions;
+export default inputSlice.reducer;
