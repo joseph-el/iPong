02273fd72039fbb668521a0fa80467b1729ae7b2
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-11 10:37:02 +0100

    adding svg for group componenet

---
+++ frontend/srcs/components/UI/iPongChatComponents/SeeUser/SeeUser.tsx
@@ -0,0 +1,75 @@
+import React, { useEffect, useState } from "react";
+import "./SeeUser.css";
+import { SeeUsersWrapper } from "./SeeUsersWrapper";
+import { Avatar } from "@nextui-org/avatar";
+
+import InviteMatchIcon from "./InviteMatch.svg";
+import SeeProfileIcon from "./SeeProfile.svg";
+
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../state/store";
+import { useNavigate } from "react-router-dom";
+import Close from "../../Button/CloseButton/CloseButton";
+export default function SeeUser(props) {
+  const selectedMessage = useSelector(
+    (state: RootState) =>
+      state.iPongChat.ListMessages.find((message) => message.isSelect) || null
+  );
+  const navigate = useNavigate();
+
+  const handelInviteMatch = () => {
+    // TODO: Implement invite match
+  };
+
+  return (
+    <SeeUsersWrapper>
+      <div className="SeeUser-frame">
+        <div className="User-info">
+          <Avatar
+            isBordered
+            className="User-avatar w-24 h-24"
+            src={selectedMessage?.avatar}
+          />
+
+          <div className="info">
+            <div className="User-name">{selectedMessage?.fullname}</div>
+            <div className="ipongchar">iPong</div>
+          </div>
+        </div>
+
+        <Close
+          func={() => props.handleCloseClick()}
+          ClassName={"Customize-chat-icon animate-pulse"}
+          id="close"
+        />
+
+        <div className="User-options">
+          <img
+            onClick={() => {
+              navigate("/ipong/profile");
+            }}
+            src={SeeProfileIcon}
+            alt="See Profile"
+            className="User-options-See-Profile-img"
+          />
+          <img
+            onClick={() => {
+              handelInviteMatch;
+            }}
+            src={InviteMatchIcon}
+            alt="Invite Match"
+            className="User-options-Invite-Match-img"
+          />
+        </div>
+
+        <div className="privacy-frame">
+          <div className="privacy-text">privacy</div>
+
+          <div className="User-options-buttons">
+            <div className="DeleteChat">Delete Chat</div>
+          </div>
+        </div>
+      </div>
+    </SeeUsersWrapper>
+  );
+}
---
+++ frontend/srcs/components/UI/iPongChatComponents/SeeUser/SeeUsersWrapper.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './SeeUser.css';
+
+export const SeeUsersWrapper = ({children}) => (
+  <div className="SeeUsers-box">
+    {children}
+  </div>
+);
