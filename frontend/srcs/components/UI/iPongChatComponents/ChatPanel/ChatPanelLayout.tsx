ff870ff6d36a6187f9f54bd8fda99b89a008275a
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-11 10:35:00 +0100

    feat: Add group chat settings and friend chat settings to chat panel

--- frontend/srcs/components/UI/iPongChatComponents/ChatPanel/ChatPanelLayout.tsx
+++ frontend/srcs/components/UI/iPongChatComponents/ChatPanel/ChatPanelLayout.tsx
@@ -6,16 +6,15 @@ import { Grid, GridItem } from "@chakra-ui/react";
 import ChatPanelHeader from "../ChatPanelHeader/ChatPanelHeader";
 import ChatPanelFooter from "../ChatPanelFooter/ChatPanelFooter";
 import ChatPanel from "./ChatPanel";
-import CreatNewMessage from "../CreatNewMessage/CreatNewMessage";
-
+import SeeGroup from "../SeeGroup/SeeGroup";
+import SeeUser from "../SeeUser/SeeUser";
 export default function ChatPanelLayout() {
-
   const [ShowGroupChatSettings, setShowGroupChat] = React.useState(false);
   const [ShowFriendChatSetting, setShowFriendChatSetting] =
     React.useState(false);
 
   const handleCloseClick = () => {
- 
+    console.log("closedd");
     setShowGroupChat(false);
     setShowFriendChatSetting(false);
   };
@@ -32,22 +31,33 @@ export default function ChatPanelLayout() {
         fontWeight="bold"
       >
         <GridItem pl="2" area={"header"}>
-          <ChatPanelHeader />
+          <ChatPanelHeader 
+            SetViewGroupSettings={() => setShowGroupChat(true)}
+            SetViewFriendSettings={() => setShowFriendChatSetting(true)}
+          />
         </GridItem>
 
         <GridItem pl="2" style={{ overflowY: "auto" }} area={"main"}>
           <ChatPanel />
-
-
-
-
-
-
-
-
-
-
         </GridItem>
+
+
+
+        {ShowGroupChatSettings && (
+          <div className="blur-background">
+            <div className="AchievementList-place fade-in">
+              <SeeGroup handleCloseClick={handleCloseClick}/>
+            </div>
+          </div>
+        )}
+
+        {ShowFriendChatSetting && (
+          <div className="blur-background">
+            <div className="AchievementList-place fade-in">
+              <SeeUser handleCloseClick={handleCloseClick} />
+            </div>
+          </div>
+        )}
 
         <GridItem pl="2" area={"Footer"}>
           <ChatPanelFooter />
