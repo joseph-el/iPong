b6d2d3032e9efef8845be5583c57bdfdab79d539
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-11 10:35:06 +0100

    Refactor: Update ChatPanelHeader component for loading selected message info
    Updates the ChatPanelHeader component to retrieve and display the selected message's information dynamically, including type, avatar, and fullname.

--- frontend/srcs/components/UI/iPongChatComponents/ChatPanelHeader/ChatPanelHeader.tsx
+++ frontend/srcs/components/UI/iPongChatComponents/ChatPanelHeader/ChatPanelHeader.tsx
@@ -1,19 +1,48 @@
 import React from "react";
 import "./ChatPanelHeader.css";
 
-import {Avatar, AvatarGroup, AvatarIcon} from "@nextui-org/react";
+import { Avatar } from "@nextui-org/react";
 import ConversationDetailsIcon from "../MessagesItems/select-converstation-icon.svg";
 
+import { useSelector } from "react-redux";
+import { RootState } from "../../../../state/store";
+import { createSelector } from 'reselect';
+
+const getListMessages = (state) => state.iPongChat.ListMessages;
+
+const getSelectedMessage = createSelector(
+  [getListMessages],
+  (listMessages) => listMessages.find((message) => message.isSelect) || null
+);
+
 export default function ChatPanelHeader(props) {
-    return (
-        <div className="ChatPanelHeader-frame">
-            <div className="ChatPanelHeader-title" onClick={()  => {props.SetViewChatSettings} }>
-                <Avatar src="https://i.pravatar.cc/150?u=a04258a2462d826712d" className="w-14 h-14"/>
-                <div className="conversation-name-and-conversation-details">
-                    <div className="conversation-name">Taha Naceur</div>
-                    <img  src={ConversationDetailsIcon} alt="conversation-details" className="conversation-details"/>
-                </div>
-            </div>
+  const selectedMessage = useSelector(getSelectedMessage);
+
+  return (
+    <div className="ChatPanelHeader-frame">
+      <div
+        className="ChatPanelHeader-title"
+        onClick={() => {
+            console.log(selectedMessage);
+            console.log(selectedMessage.type);
+            console.log(selectedMessage.fullname);
+            (selectedMessage.type === "group") && props.SetViewGroupSettings();
+            (selectedMessage.type === "Dm")    && props.SetViewFriendSettings();
+        }}
+      >
+        <Avatar
+          src={selectedMessage?.avatar}
+          className="w-14 h-14"
+        />
+        <div className="conversation-name-and-conversation-details">
+          <div className="conversation-name">{selectedMessage?.fullname}</div>
+          <img
+            src={ConversationDetailsIcon}
+            alt="conversation-details"
+            className="conversation-details"
+          />
         </div>
-    )
+      </div>
+    </div>
+  );
 }
