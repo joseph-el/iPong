44ebb86a32477b94f059aa08ee00e59064191eca
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-22 15:30:37 +0100

    Refactor: Integrate notifications service in message module

--- backend/src/messages/message.module.ts
+++ backend/src/messages/message.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { MessageService } from './message.service';
 import { MessageController } from './message.controller';
 import { DatabaseService } from 'src/database/database.service';
+import { NotificationsService } from 'src/notifications/notifications.service';
 
 @Module({
   controllers: [MessageController],
-  providers: [MessageService, DatabaseService],
+  providers: [MessageService, DatabaseService ,NotificationsService,
+  ],
 })
 export class MessageModule {}
