f72bd673bc6d36491a67147f5ccc0edd9b1f0e8b
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-26 11:58:46 +0100

    Fix: Use Redux store for user avatar
    Adds avatar path to Redux store and updates VersusBadge to use it.

--- frontend/srcs/game/components/VersusBadge/VersusBadge.tsx
+++ frontend/srcs/game/components/VersusBadge/VersusBadge.tsx
@@ -1,6 +1,9 @@
 import React from "react";
 import VersusBadgeImg from "/assets/game/gameLayout/versus-badge.svg";
 import "./VersusBadge.css";
+import {Avatar} from "@nextui-org/avatar";
+import { useSelector } from "react-redux";
+import { RootState } from "../../../state/store";
 
 interface VersusBadgeProps {
   userName: string;
@@ -11,12 +14,16 @@ export default function VersusBadge({
   userName,
   avatarPath,
 }: VersusBadgeProps) {
+
+
+  const UserInfo = useSelector((state: RootState) => state.userState);
   return (
     <div className="iPongGame-VersusBadge">
       <img src={VersusBadgeImg} alt="VersusBadge" className="VersusBadge" />
       <div className="iPongGame-UserBadge-username">
         <img
-          src={avatarPath}
+        
+          src={UserInfo.picture}
           alt="UserAvatar"
           className="UserBadge-img UserBadge-img-vr"
         />
