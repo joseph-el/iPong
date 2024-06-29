92efe59d4732ca641ccf039d50792d79954a3205
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-29 23:12:51 +0100

    Refactor: Update UserBadge component with Next UI styles

--- frontend/srcs/game/components/UserBadge/UserBadge.tsx
+++ frontend/srcs/game/components/UserBadge/UserBadge.tsx
@@ -5,23 +5,19 @@ import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../../state/store";
 import {Avatar} from "@nextui-org/avatar";
 
-export default function UserBadge({playerScore}) {
+export default function UserBadge({playerScore, SelectedBackground}) {
   const UserInfo = useSelector((state: RootState) => state.userState);
 
   return (
     <div className="iPongGame-UserBadge">
-      <img src={UserBadgeImg} alt="UserBadge" className="UserBadge" />
-      <div className="iPongGame-UserBadge-username">
-        <img
-          
-          src={UserInfo.picture}
-          alt="UserAvatar"
-   
-          className="UserBadge-img"
-        />
-        <div className="username">{UserInfo.username}</div>
-        <div className="score">{playerScore}</div>
-      </div>
+      <Avatar
+        radius="lg"
+        src={UserInfo.picture}
+      />
+      <p className="iPongGame-UserBadge-username">{UserInfo.username}</p>
+
+      <p className="iPongGame-UserBadge-score">{playerScore}</p>
+
     </div>
   );
 }
