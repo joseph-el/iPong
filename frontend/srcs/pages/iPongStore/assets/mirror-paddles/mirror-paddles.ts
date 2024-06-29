f70f966c587c0825250a9f635d794926311a2f7b
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-29 17:22:17 +0100

    feat(iPongStore): Add new mirror paddles
    Adds AlbertEinstein, BloodSucker, Ice, Ninja, Spiderman, Archer, DesertMan, Joker, Pharaoh, UaeMan, Astronaut, Hacker, LaCasaDePapel, SaudiMan, and Zombie mirror paddles to the iPongStore.

---
+++ frontend/srcs/pages/iPongStore/assets/mirror-paddles/mirror-paddles.ts
@@ -0,0 +1,35 @@
+// Import all mirrored paddle images
+import { default as AlbertEinsteinMirror } from './Albert Einstein-mirror.png';
+import { default as BloodSuckerMirror } from './Blood Sucker-mirror.png';
+import { default as IceMirror } from './Ice-mirror.png';
+import { default as NinjaMirror } from './Ninja-mirror.png';
+import { default as SpidermanMirror } from './Spiderman-mirror.png';
+import { default as ArcherMirror } from './Archer-mirror.png';
+import { default as DesertManMirror } from './Desert Man-mirror.png';
+import { default as JokerMirror } from './Joker-mirror.png';
+import { default as PharaohMirror } from './Pharaoh-mirror.png';
+import { default as UaeManMirror } from './Uae Man-mirror.png';
+import { default as AstronautMirror } from './Astronaut-mirror.png';
+import { default as HackerMirror } from './Hacker-mirror.png';
+import { default as LaCasaDePapelMirror } from './La Casa De Papel-mirror.png';
+import { default as SaudiManMirror } from './Saudi man-mirror.png';
+import { default as ZombieMirror } from './Zombie-mirror.png';
+
+
+export {
+  AlbertEinsteinMirror,
+  BloodSuckerMirror,
+  IceMirror,
+  NinjaMirror,
+  SpidermanMirror,
+  ArcherMirror,
+  DesertManMirror,
+  JokerMirror,
+  PharaohMirror,
+  UaeManMirror,
+  AstronautMirror,
+  HackerMirror,
+  LaCasaDePapelMirror,
+  SaudiManMirror,
+  ZombieMirror,
+};
