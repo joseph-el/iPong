80b65423fda2d622ce9efa1863e06bd318ecab17
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-30 12:32:11 +0100

    Fix(game): Refactor connection error component to use iPongAlert
    Co-authored-by: Issam Loubna <issam@pinger.app>

--- frontend/srcs/game/components/ConnectionError/ConnectionError.tsx
+++ frontend/srcs/game/components/ConnectionError/ConnectionError.tsx
@@ -2,7 +2,8 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import "./ConnectionError.css";
 import { PATHS } from "../../constants/paths";
-
+import IPongAlert from "../../../components/UI/iPongAlert/iPongAlert";
+import { useDisclosure } from "@nextui-org/react";
 interface ErrorConnectionProps {
   reason: string;
 }
@@ -14,16 +15,31 @@ export default function ErrorConnection({ reason }: ErrorConnectionProps) {
   useEffect(() => {
     const timer = setTimeout(() => {
       setVisible(false);
-      navigate(PATHS.DEFAULT_GAME_PAGE);
+      // navigate(PATHS.DEFAULT_GAME_PAGE);
     }, 2000);
 
     return () => clearTimeout(timer);
   }, [navigate]);
 
-  return visible ? (
-    <div className="errorCard">
-      <h1>Error connecting to server...</h1>
-      <h3>{reason}</h3>
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  useEffect(() => {
+    visible && onOpen();
+  }, [visible, onOpen]);
+  return (
+    <div>
+      <IPongAlert
+        handelRemoveUser={() => {
+          onClose();
+
+          navigate(PATHS.DEFAULT_GAME_PAGE);
+        }}
+        hideCloseButton={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        UserAlertHeader={"Error connecting to server"}
+        UserAlertMessage={reason}
+        UserOptions={"Close"}
+      />
     </div>
-  ) : null;
+  );
 }
