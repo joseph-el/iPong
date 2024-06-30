d83238c16459f4e5b7c2fc84aadc4bf39e8d435e
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-30 12:33:52 +0100

    update Looking for match

---
+++ frontend/srcs/pages/iPongStore/db/mirror.db.ts
@@ -0,0 +1,95 @@
+import * as MirrorPaddlesPNGs from "../assets/mirror-paddles/mirror-paddles";
+import { Skin } from '../interfaces/skin.interface';
+
+export const MIRROR_SKIN_DB: Skin[] = [
+  {
+    name: 'Archer',
+    description: 'Swift and precise, this skin is inspired by the legendary archer.',
+    imgPath: MirrorPaddlesPNGs.ArcherMirror,
+    price: 5,
+  },
+  {
+    name: 'Pharaoh',
+    description: 'Rule the waves with this majestic skin, fit for ancient royalty.',
+    imgPath: MirrorPaddlesPNGs.PharaohMirror,
+    price: 30,
+  },
+  {
+    name: 'Uae Man',
+    description: 'Celebrate the spirit of unity with this skin, representing cultural heritage.',
+    imgPath: MirrorPaddlesPNGs.UaeManMirror,
+    price: 100,
+  },
+  {
+    name: 'Joker',
+    description: 'Add a touch of mischief to your ride with this playful skin, guaranteed to bring smiles.',
+    imgPath: MirrorPaddlesPNGs.JokerMirror,
+    price: 53,
+  },
+  {
+    name: 'Ninja',
+    description: 'Strike silently but stylishly with this ninja-inspired skin, perfect for stealthy moves.',
+    imgPath: MirrorPaddlesPNGs.NinjaMirror,
+    price: 157,
+  },
+  {
+    name: 'Saudi Man',
+    description: 'Ride in style with this luxurious skin, fit for royalty on the board.',
+    imgPath: MirrorPaddlesPNGs.SaudiManMirror,
+    price: 80,
+  },
+  {
+    name: 'Spiderman',
+    description: 'Swing into action with this heroic skin, inspired by everyone\'s favorite web-slinger.',
+    imgPath: MirrorPaddlesPNGs.SpidermanMirror,
+    price: 75,
+  },
+  {
+    name: 'Zombie',
+    description: 'Unleash your undead spirit with this spooky skin, perfect for Halloween shredding.',
+    imgPath: MirrorPaddlesPNGs.ZombieMirror,
+    price: 122,
+  },
+  {
+    name: 'Ice',
+    description: 'Embrace the chill with this icy skin, perfect for cool maneuvers on the board.',
+    imgPath: MirrorPaddlesPNGs.IceMirror,
+    price: 90,
+  },
+  {
+    name: 'Hacker',
+    description: 'Unleash your inner hacker with this tech-inspired skin, perfect for cybernetic tricks.',
+    imgPath: MirrorPaddlesPNGs.HackerMirror,
+    price: 110,
+  },
+  {
+    name: 'Albert Einstein',
+    description: 'Channel genius on the board with this skin inspired by the iconic scientist.',
+    imgPath: MirrorPaddlesPNGs.AlbertEinsteinMirror,
+    price: 60,
+  },
+  {
+    name: 'La Casa De Papel',
+    description: 'Join the heist with this skin inspired by the famous TV show.',
+    imgPath: MirrorPaddlesPNGs.LaCasaDePapelMirror,
+    price: 145,
+  },
+  {
+    name: 'Desert Man',
+    description: 'Conquer sandy terrains with this skin inspired by desert landscapes.',
+    imgPath: MirrorPaddlesPNGs.DesertManMirror,
+    price: 100,
+  },
+  {
+    name: 'Astronaut',
+    description: 'Embark on a space adventure with this futuristic skin.',
+    imgPath: MirrorPaddlesPNGs.AstronautMirror,
+    price: 85,
+  },
+  {
+    name: 'Blood Sucker',
+    description: 'Unleash your dark side with this skin inspired by mythical creatures.',
+    imgPath: MirrorPaddlesPNGs.BloodSuckerMirror,
+    price: 85,
+  },
+];
