d7c1baa1a7f2a4d7c98d4d9d4ee2fed9391d5418
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-06-30 12:32:07 +0100

    Refactor: Display pop-up alert on match cancellation

--- frontend/srcs/game/components/CancelledMatch/CancelledMatch.tsx
+++ frontend/srcs/game/components/CancelledMatch/CancelledMatch.tsx
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useFetcher, useNavigate } from "react-router-dom";
 import { PATHS } from "../../constants/paths";
 import "./CancelledMatch.css";
 
+import IPongAlert from "../../../components/UI/iPongAlert/iPongAlert";
+import { useDisclosure } from "@nextui-org/react";
 interface CancelledMatchProps {
   WhyReason: string;
 }
@@ -14,16 +16,32 @@ export default function CancelledMatch({ WhyReason }: CancelledMatchProps) {
   useEffect(() => {
     const timer = setTimeout(() => {
       setVisible(false);
-      navigate(PATHS.DEFAULT_GAME_PAGE);
+
+      // navigate(PATHS.DEFAULT_GAME_PAGE);
     }, 4000);
 
     return () => clearTimeout(timer);
   }, [navigate]);
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  useEffect(() => {
+    visible && onOpen();
+  }, [visible, onOpen]);
 
-  return visible ? (
-    <div className="cancelledMatchCard">
-      <h2>Match Cancelled</h2>
-      <p>{WhyReason}</p>
+  return (
+    <div>
+      <IPongAlert
+        handelRemoveUser={() => {
+          onClose();
+
+          navigate(PATHS.DEFAULT_GAME_PAGE);
+        }}
+        hideCloseButton={false}
+        isOpen={isOpen}
+        onClose={onClose}
+        UserAlertHeader={"Match Cancelled"}
+        UserAlertMessage={WhyReason}
+        UserOptions={"Close"}
+      />
     </div>
-  ) : null;
+  );
 }
