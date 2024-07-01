3822629f9d66334ae27a3c275bca27c9cc7aaa8a
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-07-01 18:20:09 +0100

    Refactor: Update CongratulationsNewSkin component
    - Rewrite UI
    - Improve styles and UX
    - Update props and classes

--- frontend/srcs/components/UI/CongratulationsNewItem/CongratulationsNewSkin.tsx
+++ frontend/srcs/components/UI/CongratulationsNewItem/CongratulationsNewSkin.tsx
@@ -5,67 +5,57 @@ import Close from "../Button/CloseButton/CloseButton";
 import { Button, Image, Tooltip } from "@nextui-org/react";
 import { Card, CardFooter, CardBody, CardHeader } from "@nextui-org/react";
 
-const CongratulationsNewSkinNavbar = (props) => {
-  const { level } = props;
-  return (
-    <div className="CongratulationsNewSkinNavbar">
-      <div className="overlap-group">
-        <div className="text-wrapper">Congratulations</div>
-        <div className="push-button">
-          <Close func={props.close} id="close" />
-        </div>
-      </div>
-    </div>
-  );
-};
 
 export default function CongratulationsNewSkin(props) {
   return (
-    <CongratulationsNewSkinWrapper>
-      <CongratulationsNewSkinNavbar close={props.close} />
-      <div className="CongratulationsNewSkin-frame">
-        <Card isFooterBlurred radius="lg" className="border-none h-[100%]">
-          <Image
-            alt="Woman listing to music"
-            className="object-cover w-full h-[400px] rounded-t-lg"
-            src={props.imgPath}
-          />
-          <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
-            <p className="text-tiny text-white/80">{props.name}</p>
-            <Tooltip
-              size={"lg"}
-              placement={"bottom"}
-              className="w-[250px] bg-black"
-              content={props.description}
-              delay={10}
-            >
-              <Button
-                className="text-tiny text-white bg-black/20"
-                variant="flat"
-                color="default"
-                radius="lg"
-                size="sm"
-              >
-                {"description"}
-              </Button>
-            </Tooltip>
-          </CardFooter>
-        </Card>
-        {/* <Image
-          shadow="none"
-          radius="none"
-          width="100%"
+
+    <div className="CongratulationsNewSkin-frame">
+      <Card
+        isFooterBlurred
+        radius="lg"
+        className="CongratulationsNewSkin-card h-full w-[400px] set-padding"
+      >
+        <Image
           alt={"SKIN IMG"}
-          className="w-[40%] h-[400px] animate-pulse"
+          className={
+            "object-cover w-full h-[550px]   " +
+            (props.BuyType === "skin" ? "adding-padding " : "")
+          }
           src={props.imgPath}
         />
 
-        <Tooltip size={"lg"} placement={"bottom"} className="w-[250px] bg-black" content={props.description} delay={10}>
-          <Button size="md"  radius="lg" className="Tooltip-Button bg-black border-1"  variant="flat">
-            {props.name}
-          </Button>
-        </Tooltip> */}
-      </div>
-    </CongratulationsNewSkinWrapper>
+        <CardHeader className="header-">
+        <div className="text-wrapper">Congratulations</div>
+          <div className="push-button">
+         
+            <Close func={props.close} id="close" />
+          </div>
+        </CardHeader>
+        <CardFooter className="justify-between before:bg-white/10 border-white/20 border-1 overflow-hidden py-1 absolute rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
+          <p className="text-tiny py-2 footer-padding text-white/80">{props.name}</p>
+
+          <Tooltip
+            size={"lg"}
+            placement={"bottom"}
+            className="w-[250px] text-description"
+            content={props.description}
+            delay={10}
+          >
+            <Button
+              className="text-tiny text-white footer-button"
+              variant="flat"
+              color="default"
+              radius="lg"
+              size="sm"
+            >
+              {"description"}
+            </Button>
+          </Tooltip>
+        </CardFooter>
+      </Card>
+
+
+    </div>
+
   );
 }
