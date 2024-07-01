cedb4e94b0a57925a66e0f0ff609be0171665a68
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-07-01 18:24:49 +0100

    upgrade front-end

---
+++ frontend/srcs/game/components/InviteGameModal/InviteGameModalWrapper.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './InviteGameModal.css';
+
+export const InviteGameModalWrapper = ({children}) => (
+  <div className="InviteGameModal-box">
+    {children}
+  </div>
+);
