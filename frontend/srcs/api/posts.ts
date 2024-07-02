096be5e0770357649b9f60262d9efb445ac17e85
Youssef El idrissi <youssefelidrissi200@gmail.com>
2024-07-02 16:40:44 +0100

    Refactor: Remove unnecessary imports in posts.ts

--- frontend/srcs/api/posts.ts
+++ frontend/srcs/api/posts.ts
@@ -1,20 +1,13 @@
-import axios from 'axios';
-// import { env } from 'process';
+import axios from "axios";
 
-
-// console.log("-----ENVS-->", env);
-// import dotenv from 'dotenv';
-// dotenv.config();
-
-// console.log(dotenv);
 const url = import.meta.env.VITE_URL;
 
 const api = axios.create({
   baseURL: `http://${url}:3000`,
   withCredentials: true,
   headers: {
-    'Content-Type': 'application/json',
-    'Cache-Control': 'no-cache',
+    "Content-Type": "application/json",
+    "Cache-Control": "no-cache",
   },
 });
 
